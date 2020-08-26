2bf4af6e011d46b0f8bf5731e1b4891908311fd3
Gabriel Moya <gabrielmoya123@gmail.com>
2020-08-26 18:36:55 -0400

    Exercício 1019 URI

---
+++ codigos-uri/conversao-de-tempo.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main() {
+    int inSeg, outHoras, outMin, outSeg;
+
+    // Entrada dos dados
+    scanf("%d", &inSeg);
+
+    // Processamento dos dados
+    // outHoras = primeira divisão transforma em minutos, e os minutos transforma em horas
+    outHoras = (inSeg/60)/60;
+    // outMin = primeira divisão transforma em minutos, e o resto da divisão tira as horas (Que já calculamos)
+    outMin = (inSeg/60)%60;
+    // outSeg = O resto da divisão dos próprios segundos, vai restar os segundos
+    outSeg = inSeg%60;
+    
+    // Saída dos dados
+    printf("%d:%d:%d\n", outHoras, outMin, outSeg);
+
+    return 0;
+}
