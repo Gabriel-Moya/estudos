71a5b149b31ada0cc31e9e4421d626847be87993
Gabriel Moya <gabrielmoya123@gmail.com>
2020-08-27 23:08:30 -0400

    Exercicio 1021 URI

---
+++ codigos-uri/notas-e-moedas.c
@@ -0,0 +1,38 @@
+/*----------------------------------------*/
+/*Exercício 1021 do URI*/
+/*https://www.urionlinejudge.com.br/judge/pt/problems/view/1021*/
+/*----------------------------------------*/
+
+#include <stdio.h>
+
+int main() {
+
+    float valor, vetIn[12]={100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.10, 0.05, 0.01};
+    int i, vetOut[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    scanf("%f", &valor);
+
+    for(i=0; i<12; i++){
+        while(valor >= vetIn[i]){
+            valor -= vetIn[i];
+            vetOut[i]++;
+        }
+    }
+
+    printf("NOTAS:\n");
+    printf("%i nota(s) de R$ 100.00\n", vetOut[0]);
+    printf("%i nota(s) de R$ 50.00\n", vetOut[1]);
+    printf("%i nota(s) de R$ 20.00\n", vetOut[2]);
+    printf("%i nota(s) de R$ 10.00\n", vetOut[3]);
+    printf("%i nota(s) de R$ 5.00\n", vetOut[4]);
+    printf("%i nota(s) de R$ 2.00\n", vetOut[5]);
+    printf("MOEDAS:\n");
+    printf("%i moeda(s) de R$ 1.00\n", vetOut[6]);
+    printf("%i moeda(s) de R$ 0.50\n", vetOut[7]);
+    printf("%i moeda(s) de R$ 0.25\n", vetOut[8]);
+    printf("%i moeda(s) de R$ 0.10\n", vetOut[9]);
+    printf("%i moeda(s) de R$ 0.05\n", vetOut[10]);
+    printf("%i moeda(s) de R$ 0.01\n", vetOut[11]);
+
+    return 0;
+}
