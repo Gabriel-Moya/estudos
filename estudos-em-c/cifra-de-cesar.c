caecff6f9956d707bf1f1b07d0aeb5de46bfdfbf
Gabriel Moya <gabrielmoya123@gmail.com>
2020-08-31 22:46:44 -0400

    Cifra de césar

---
+++ estudos-em-c/cifra-de-cesar.c
@@ -0,0 +1,31 @@
+# include <stdio.h>
+
+int main () {
+
+    int desloc, i=0;
+    char palavra[30], encrypt[30];
+
+    printf("Digite a quantidade de deslocamento:\n");
+    scanf("%d", &desloc);
+
+    system("cls");
+
+    printf("Digite a palavra que quer criptografar (Limite de 30 caracteres):\n");
+    scanf("%s", &palavra);
+
+    for(i=0;i<strlen(palavra);i++) {
+        int encrypt=(int)palavra[i]+desloc;
+        printf("%c",(char)encrypt);
+    }
+
+    /*while(palavra[i] < strlen(palavra)) {
+        encrypt[i] = palavra[i]+desloc;
+        if (encrypt[i]>122)
+            encrypt[i] -= 26;
+        i++;
+    }
+
+    printf("%s", encrypt);*/
+
+    return 0;
+}
