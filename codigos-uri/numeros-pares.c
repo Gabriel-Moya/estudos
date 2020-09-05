ae401da8b4db578266182dea6fb54d24af3cfeab
Gabriel Moya <gabrielmoya123@gmail.com>
2020-09-04 23:05:24 -0400

    Exercício números pares URI

---
+++ codigos-uri/numeros-pares.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main () {
+
+    int i=2;
+
+    while(i<=100){
+        if (i%2==0)
+        {
+            printf("%d\n", i);
+            i+=2;
+        }
+    }
+
+    return 0;
+}
