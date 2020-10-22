bc08e000d2b4ca02fa4fc82908d6902a67a1cd29
Gabriel Moya <gabrielmoya123@gmail.com>
2020-10-22 02:12:36 -0300

    .gitkeep

---
+++ estudos-em-c/conversor-decimal-to-binario.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+
+    double inDecimal, outBinario;
+
+    printf("Digite um número real, com numeros apos a virgula");
+    scanf("%lf", &inDecimal);
+
+    return 0;
+}
