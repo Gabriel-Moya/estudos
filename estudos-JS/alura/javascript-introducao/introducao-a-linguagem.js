234f3ee091ef54562d06407daf925971a61f28a1
Gabriel-PC <gabrielmoya123@gmail.com>
2020-11-30 23:16:22 -0400

    Aula1 - introdução a linguagem

---
+++ estudos-JS/alura/javascript-introducao/introducao-a-linguagem.js
@@ -0,0 +1,13 @@
+console.log("Primeiro programa: trabalhando com variáveis");
+
+const idade = 26;
+
+console.log("idade")
+console.log(idade);
+console.log(idade+2);
+console.log(idade-2);
+console.log(idade/2);
+console.log(idade*2);
+
+const idadeSomada = idade+2;
+console.log(idadeSomada);
