e7451c59dd94f641e229c5dd05e1a7df63587246
Gabriel-PC <gabrielmoya123@gmail.com>
2020-12-01 18:26:27 -0400

    Aula3 - Trabalhando com muitos elementos

---
+++ estudos-JS/alura/javascript-introducao/listas.js
@@ -0,0 +1,17 @@
+console.log(`Trabalhando com listas`);
+
+const destinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`
+);
+
+console.log(destinos);
+
+destinos.push(`Curitiba`)
+
+console.log(destinos);
+
+destinos.splice(1,1);
+
+console.log(destinos);
