b5d5ad239cec85c41e87ca3e7b6e4df59c8e0dbb
Gabriel-PC <gabrielmoya123@gmail.com>
2020-12-02 14:58:37 -0400

    Aula3 - JavaScript POO

---
+++ estudos-JS/alura/javascript-poo/ContaCorrente.js
@@ -0,0 +1,25 @@
+export class ContaCorrente{
+    agencia;
+    cliente;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+
+        this._saldo += valor;
+    }
+
+    transferir(valor, conta){
+        const valorSacado = this.sacar(valor);
+        conta.depositar(valorSacado);
+    }
+}
--- estudos-JS/alura/javascript-poo/index.js
+++ estudos-JS/alura/javascript-poo/index.js
@@ -1,45 +1,25 @@
-class Cliente{
-    nome;
-    cpf;
-}
-
-class ContaCorrente{
-    agencia;
-    _saldo = 0;
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0){
-            return;
-        }
-
-        this._saldo += valor;
-    }
-}
+import {Cliente} from "./Cliente.js"
+import {ContaCorrente} from "./ContaCorrente.js"
 
 const cliente1 = new Cliente();
 cliente1.nome = "Gabriel";
 cliente1.cpf = 11122233309;
 
-
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
 
-const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.agencia = 1001;
+const contaCorrenteGabriel = new ContaCorrente();
+contaCorrenteGabriel.agencia = 1001;
+contaCorrenteGabriel.cliente = cliente1;
+contaCorrenteGabriel.depositar(500);
 
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
+const conta2 = new ContaCorrente();
+conta2.cliente = cliente2;
+conta2.agencia = 102;
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+let valor = 200;
+contaCorrenteGabriel.transferir(valor, conta2);
 
-console.log(contaCorrenteRicardo);
+console.log("valor: ", valor);
+console.log(conta2);
