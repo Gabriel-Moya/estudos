3d9fea68c823459af2b9057d0eb6db1a4b428f64
Gabriel Moya <gabrielmoya123@gmail.com>
2021-02-01 23:00:35 -0400

    Menu de navegação

--- exercicio.php
+++ exercicio.php
@@ -6,15 +6,21 @@
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="assets/css/style.css">
+        <link rel="stylesheet" href="assets/css/exercicio.css">
         <title>Curso PHP7 - Cod3r</title>
     </head>
 
-    <body>
+    <body class="exercicio">
 
         <header class="cabecalho">
             <h1>Curso PHP</h1>
             <h2>Visualização do exercício</h2>
         </header>
+
+        <nav class="navegacao">
+            <a href="#" class="verde">Sem formatação</a>
+            <a href="index.php" class="vermelho">Voltar</a>
+        </nav>
 
         <main class="principal">
             <div class="conteudo">
