66ba7eef139471e9cd77f0db1fbe1854c8ed5183
Gabriel Moya <gabrielmoya123@gmail.com>
2021-02-01 22:44:47 -0400

    Visualização do exercício

---
+++ exercicio.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+    <head>
+        <meta charset="UTF-8">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="assets/css/style.css">
+        <title>Curso PHP7 - Cod3r</title>
+    </head>
+
+    <body>
+
+        <header class="cabecalho">
+            <h1>Curso PHP</h1>
+            <h2>Visualização do exercício</h2>
+        </header>
+
+        <main class="principal">
+            <div class="conteudo">
+                
+            </div>
+        </main>
+
+        <footer class="rodape">
+            Gabriel Moya <?= date('Y'); ?>
+        </footer>
+
+    </body>
+
+</html>
