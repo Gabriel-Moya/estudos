afab476a93f8948da38063e64168f6dd98e5bcc9
Gabriel Moya <gabrielmoya123@gmail.com>
2021-03-11 19:11:48 -0400

    Aula 3

---
+++ faculdade/ads-uninter/logica-e-algoritmos/aula3-estruturas-selecao/condicional-simples.py
@@ -0,0 +1,13 @@
+# Não será atribuído nenhum valor as variáveis por se tratar somente de fixação de conceitos
+
+# Se idade é maior que 60, escreva: "Você tem direito aos benefícios"
+if (idade > 60):
+    print('Você tem direito aos benefícios')
+
+# Se dano é maior que 10 e escudo é igual a 0, escreva: "Você está morto!"
+if (dano > 10 and escudo == 0):
+    print('Você está morto!')
+
+# Se pelo menos uma das variáveis booleanas norte, sul, leste e oeste resultarem em True, escreva: "Você escapou!"
+if (norte or sul or leste or oeste):
+    print('Você escapou')
---
+++ faculdade/ads-uninter/logica-e-algoritmos/aula3-estruturas-selecao/expressoes-booleanas.py
@@ -0,0 +1,25 @@
+# Exercício A
+print(2 + 2 < 4)
+
+# Exercício B
+print(7 // 3 == 1 + 1)
+
+# Exercício C
+print(3**2 + 4**2 == 25)
+
+# Exercício D
+print(2 + 4 + 6 > 12)
+
+###########################################################
+# Escreva as seguintes expressões booleanas
+#
+# A) 1387 é divisível por 19
+# B) 31 é par
+# C) O menor valor entre 34, 29 e 31 é menor do que 30
+###########################################################
+
+print('A) ', (1387 % 2) == 0)
+
+print('B) ', (31 % 2) == 0)
+
+print('C) ', (min(34, 29, 31)) < 30)
