93801e0518967b4db9988225029321f780a36937
Gabriel Moya <gabrielmoya123@gmail.com>
2021-03-11 21:49:48 -0400

    Correção exercício 1 aula 3

--- faculdade/ads-uninter/logica-e-algoritmos/aula3-estruturas-selecao/aula3-exercicio1.py
+++ faculdade/ads-uninter/logica-e-algoritmos/aula3-estruturas-selecao/aula3-exercicio1.py
@@ -4,9 +4,15 @@ a = float(input('Insira o valor do primeiro lado: '))
 b = float(input('Insira o valor do segundo lado: '))
 c = float(input('Insira o valor do terceiro lado: '))
 
-if (a == b and b == c):
-    print('Triângulo equilátero')
-elif ((a == b and b != c) or (b == c and c != a) or (a == c and b != c)):
-    print('Triângulo isósceles')
-elif (a != b and b != c):
-    print('Triângulo escaleno')
+if (a > 0) and (b > 0) and (c > 0):
+    if (a + b > c) and (a + c > b) and (b + c > a):
+        if (a == b and b == c):
+            print('Triângulo equilátero')
+        elif ((a == b and b != c) or (b == c and c != a) or (a == c and b != c)):
+            print('Triângulo isósceles')
+        elif (a != b and b != c):
+            print('Triângulo escaleno')
+    else:
+        print('Ao menos um dos valores indicados não servem para formar um triângulo')
+else:
+    print('Ao menos um dos valores indicados não servem para formar um triângulo')
