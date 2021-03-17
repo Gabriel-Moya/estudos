35aa404febce1cd74651e8b67c8754fc469efa33
Gabriel Moya <gabrielmoya123@gmail.com>
2021-03-16 22:40:07 -0400

    Laço repetição exercício 1

--- faculdade/ads-uninter/logica-e-algoritmos/pratica/exercicio1.py
+++ faculdade/ads-uninter/logica-e-algoritmos/pratica/exercicio1.py
@@ -1,26 +1,37 @@
 import os
+import platform
+
 def cls():
-    os.system('clear')
+    if platform.system() == 'Windows':
+        os.system('cls')
+    elif platform.system() == 'Linux':
+        os.system('clear')
 
-nome = input('Digite o nome do lutador: ')
-peso = float(input('Digite o peso (em kg): '))
+select = 1
+while select != 0:
+    select = int(input('Digite qualquer número para continuar ou 0 para encerrar: '))
+    if select == 0:
+        print('Finalizando...')
+        break
+    nome = input('Digite o nome do lutador: ')
+    peso = float(input('Digite o peso (em kg): '))
 
-if (peso < 65):
-    categoria = ('Pena')
-elif (peso >= 65 and peso < 72):
-    categoria = ('Leve')
-elif (peso >= 72 and peso < 79):
-    categoria = ('Ligeiro')
-elif (peso >= 79 and peso < 86):
-    categoria = ('Meio-médio')
-elif (peso >= 86 and peso < 93):
-    categoria = ('Médio')
-elif (peso >= 93 and peso < 100):
-    categoria = ('Meio-pesado')
-elif (peso >= 100):
-    categoria = ('Pesado')
+    if (peso < 65):
+        categoria = ('Pena')
+    elif (peso >= 65 and peso < 72):
+        categoria = ('Leve')
+    elif (peso >= 72 and peso < 79):
+        categoria = ('Ligeiro')
+    elif (peso >= 79 and peso < 86):
+        categoria = ('Meio-médio')
+    elif (peso >= 86 and peso < 93):
+        categoria = ('Médio')
+    elif (peso >= 93 and peso < 100):
+        categoria = ('Meio-pesado')
+    elif (peso >= 100):
+        categoria = ('Pesado')
 
-cls()
+    cls()
 
-print('\nNome fornecido: {}\nPeso fornecido: {}'.format(nome, peso))
-print('O lutador {} pesa {}kg e se enquadra na categoria {}'.format(nome, peso, categoria))
+    print('\nNome fornecido: {}\nPeso fornecido: {}'.format(nome, peso))
+    print('O lutador {} pesa {}kg e se enquadra na categoria {}'.format(nome, peso, categoria))
