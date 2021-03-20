66d7366d1a1bab3ebe8fab0207da2e8f22ac3a1a
Gabriel Moya <gabrielmoya123@gmail.com>
2021-03-20 19:33:16 -0400

    Correção exercício 2 aula 3

--- faculdade/ads-uninter/logica-e-algoritmos/aula3-estruturas-selecao/aula3-exercicio2.py
+++ faculdade/ads-uninter/logica-e-algoritmos/aula3-estruturas-selecao/aula3-exercicio2.py
@@ -8,19 +8,17 @@ print('/ Divisão')
 print('Pressione outra tecla para sair')
 
 op = input('Qual operação deseja fazer? ')
-if op != ('+' or '-' or '*' or '/'):
-    print('Finalizando...')
-else:
+if op == '+' or '-' or '*' or '/':
     x = float(input('Digite o primeiro valor: '))
     y = float(input('Digite o segundo valor: '))
 
-    if (op == '+'):
-        print('{} + {} = {}'.format(x,y,x+y))
-    elif (op == '-'):
-        print('{} - {} = {}'.format(x,y,x-y))
-    elif (op == '*'):
-        print('{} * {} = {}'.format(x,y,x*y))
-    elif (op == '/'):
-        print('{} / {} = {}'.format(x,y,x/y))
-    else:
-        print('Finalizando sua calculadora')
+if (op == '+'):
+    print('{} + {} = {}'.format(x,y,x+y))
+elif (op == '-'):
+    print('{} - {} = {}'.format(x,y,x-y))
+elif (op == '*'):
+    print('{} * {} = {}'.format(x,y,x*y))
+elif (op == '/'):
+    print('{} / {} = {}'.format(x,y,x/y))
+else:
+    print('Finalizando sua calculadora')
