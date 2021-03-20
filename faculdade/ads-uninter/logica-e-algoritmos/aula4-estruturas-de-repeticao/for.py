86e2d9287d055d6270d62215338bbe77eb0e0e37
Gabriel Moya <gabrielmoya123@gmail.com>
2021-03-20 00:52:54 -0400

    Aula 4 - laço for

---
+++ faculdade/ads-uninter/logica-e-algoritmos/aula4-estruturas-de-repeticao/for.py
@@ -0,0 +1,11 @@
+print('Primeira forma...')
+for i in range (6):
+    print(i)
+
+print('\nSegunda forma...')
+for i in range (1, 6, 2):
+    print(i)
+
+print('\nTerceira forma...')
+for i in range (10, 0, -2):
+    print(i)
