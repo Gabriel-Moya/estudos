841b313a114e6b59e30e403cb02a65909665b052
Gabriel Moya <gabrielmoya123@gmail.com>
2021-03-22 23:56:08 -0400

    Help function e exercicio 1 da aula 5

---
+++ faculdade/ads-uninter/logica-e-algoritmos/aula5-funcoes/aula5-exercicio1.py
@@ -0,0 +1,27 @@
+# Escreva uma função que calcule o fatorial de um número recebido como parâmetro e retorne seu resultado
+# Faça uma validação dos dados por meio de uma outra função, permitindo que somente valores positivos sejam aceitos
+# Crie o help da sua função
+
+def calculaFatorial(num):
+    """
+    Calcula fatorial de um número
+    :param num: Recebe como parâmetro o número a ser calculado o fatorial
+    :return: Retorna o resultado do fatorial
+    """
+    fatorial = 1
+    if num == 0:
+        return fatorial
+
+    for i in range (1, num+1):
+        fatorial *= i
+
+    return fatorial
+
+def validaInt(pergunta, min, max):
+    x = int(input(pergunta))
+    while ((x < min) or (x > max)):
+        x = int(input(pergunta))
+
+x = validaInt('Digite um número para calcular o fatorial: ', 0, 99999)
+print('{}! = {}'.format(x, calculaFatorial(x)))
+# help(calculaFatorial)
---
+++ faculdade/ads-uninter/logica-e-algoritmos/aula5-funcoes/criando-help.py
@@ -0,0 +1,12 @@
+def soma(x=0, y=0, z=0):
+    return x+y+z
+    """
+    Função que soma até 3 valores inteiros
+    :param x: Valor inteiro opcional
+    :param y: Valor inteiro opcional
+    :param z: Valor inteiro opcional
+    :return: Soma inteira
+    """
+
+print(soma(3,2))
+help(soma)
