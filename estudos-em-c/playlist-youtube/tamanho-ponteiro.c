43564e60688c8dfddc8004a71ddfa9f36aef3a21
Gabriel Moya <gabrielmoya123@gmail.com>
2021-04-25 00:46:47 -0400

    Ponteiros playlist

---
+++ estudos-em-c/playlist-youtube/tamanho-ponteiro.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int *p;
+    char *p2;
+    float *p3;
+
+    printf("Tamanho ponteiro int: %d\n", sizeof(p));
+    printf("Tamanho ponteiro char: %d\n", sizeof(p2));
+    printf("Tamanho ponteiro float: %d\n", sizeof(p3));
+
+    return 0;
+}
