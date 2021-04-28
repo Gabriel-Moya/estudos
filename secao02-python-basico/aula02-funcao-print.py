2df08ca1bb4839cda10fc553ef55de5c81d0d649
Gabriel Moya <gabrielmoya123@gmail.com>
2021-04-27 23:49:35 -0400

    Initial commit python

---
+++ secao02-python-basico/aula02-funcao-print.py
@@ -0,0 +1,10 @@
+# sep='-'   indica o separador, substituindo o espaço
+# end=''    indica o que será impresso no final, e não irá quebrar a linha
+
+print('Gabriel Moya')
+print('Gabriel', 'Moya')
+print('Gabriel', 'Moya', sep='-')
+
+# Imprimir um CPF com a máscara 123.456.789-01
+print('123', '456', '789', sep='.', end='-')
+print('01')
