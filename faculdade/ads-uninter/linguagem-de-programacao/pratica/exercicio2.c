487caea5f40a063cb860458060ddf58899212b92
Gabriel Moya <gabrielmoya123@gmail.com>
2021-05-15 19:28:18 -0400

    Finalização exercício 2

--- faculdade/ads-uninter/linguagem-de-programacao/pratica/exercicio2.c
+++ faculdade/ads-uninter/linguagem-de-programacao/pratica/exercicio2.c
@@ -6,19 +6,21 @@ int main(){
     int ru[7], *ptr;
     int soma = 0;
 
+    // Captura de dados
     printf("Insira seu RU: ");
-    
     for (int i = 0; i < 7; i++){
         scanf("%d", &ru[i]);
     }
 
     ptr = &ru;
 
+    // Soma dos dígitos
     for (int i = 0; i < 7; i++){
         soma += *ptr;
         ptr++;
     }
 
+    // Exibe a soma
     printf("\nA soma dos elementos resulta em: %d", soma);
 
     return 0;
