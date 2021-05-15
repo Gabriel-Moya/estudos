a5f54d6c7701adb5d1bb9bea762cd7fb3667f3e4
Gabriel Moya <gabrielmoya123@gmail.com>
2021-05-15 19:27:08 -0400

    Finalização exercício 1

--- faculdade/ads-uninter/linguagem-de-programacao/pratica/exercicio1.c
+++ faculdade/ads-uninter/linguagem-de-programacao/pratica/exercicio1.c
@@ -10,9 +10,6 @@ de 3.751,06 até 4.664,68        22,5%
 a partir de 4.664,68            27,5%
 */
 
-float verificaAliquotas(float);
-float calculaImposto(float, float);
-
 int main(){
 
     float aliquota, imposto;
@@ -23,6 +20,7 @@ int main(){
     };
     struct cadastroFuncionario funcionario, *p;
 
+    // Captura de dados
     printf("Digite o nome do funcionario: ");
     fgets(funcionario.nome, 50, stdin);
     printf("Digite o salario do funcionario: ");
@@ -30,11 +28,8 @@ int main(){
 
     p = &funcionario;
 
-    aliquota = verificaAliquotas((*p).salario);
-    imposto = calculaImposto((*p).salario, aliquota);
-
     //Verifica alíquotas
-    /* if ((*p).salario <= 1903.98){
+    if ((*p).salario <= 1903.98){
         aliquota = 0;
     } else if ((*p).salario >= 1903.99 && (*p).salario <= 2826.65){
         aliquota = 7.5;
@@ -44,36 +39,17 @@ int main(){
         aliquota = 22.5;
     } else if ((*p).salario > 4664.68){
         aliquota = 27.5;
-    } */
-
-    // imposto = (((*p).salario)*aliquota)/100;
-
-    printf("%.2f %.2f", (*p).salario, imposto);
-
-    return 0;
-}
-
-float verificaAliquotas(float salario){
-
-    float aliquota;
-
-    if (salario <= 1903.98){
-        aliquota = 0;
-    } else if (salario >= 1903.99 && salario <= 2826.65){
-        aliquota = 7.5;
-    } else if (salario >= 2826.66 && salario <= 3751.05){
-        aliquota = 15;
-    } else if (salario >= 3751.06 && salario <= 4664.68){
-        aliquota = 22.5;
-    } else if (salario > 4664.68){
-        aliquota = 27.5;
     }
 
-    return aliquota;
-}
+    // Calculo do imposto
+    imposto = (((*p).salario)*aliquota)/100;
 
-float calculaImposto(float salario, float aliquota){
-    float imposto;
-    imposto = (salario*aliquota)/100;
-    return imposto;
+    // Exibe dados formatados
+    printf("\nNome completo: %s", (*p).nome);
+    printf("Salario: %.2f", (*p).salario);
+    printf("\nAliquota: %.2f", aliquota);
+    printf("\nValor a ser deduzido: %.2f", imposto);
+    printf("\nSalario descontado imposto: %.2f", ((*p).salario)-imposto);
+
+    return 0;
 }
