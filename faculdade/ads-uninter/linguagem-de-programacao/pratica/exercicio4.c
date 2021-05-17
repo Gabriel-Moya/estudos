21559d46d809d871f8e6dfb36a5e9e5b7babdf38
Gabriel Moya <gabrielmoya123@gmail.com>
2021-05-16 23:05:06 -0400

    Exercício 4

---
+++ faculdade/ads-uninter/linguagem-de-programacao/pratica/exercicio4.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+// Função recursiva
+int multiplica(int a, int b){
+
+    if(b == 1){
+        return a;
+    }
+
+    return a + multiplica(a, --b);
+}
+
+int main() {
+
+    int x, y, resultado;
+
+    printf("Digite dois numeros para realizar uma multiplicacao");
+
+    // Captura as informações necessárias
+    printf("\nBase: ");
+    scanf("%d", &x);
+    printf("Expoente: ");
+    scanf("%d", &y);
+
+    // Chama a função recursiva e armazena o retorno
+    resultado = multiplica(x, y);
+
+    // Exibe o resultado da operação
+    printf("Resultado: %d", resultado);
+
+    return 0;
+}
