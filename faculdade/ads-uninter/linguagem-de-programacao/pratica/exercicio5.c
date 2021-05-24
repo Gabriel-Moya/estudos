1ef4a3d7289e79a23f3a253d96a14b33a606d344
Gabriel Moya <gabrielmoya123@gmail.com>
2021-05-23 20:04:06 -0400

    Correções finais para entrega do trabalho

--- faculdade/ads-uninter/linguagem-de-programacao/pratica/exercicio5.c
+++ faculdade/ads-uninter/linguagem-de-programacao/pratica/exercicio5.c
@@ -8,8 +8,8 @@ int main() {
 
     struct dados {
         char nome[40];
-        char CPF[11];
-        char telefone[11];
+        char CPF[15];
+        char telefone[15];
         char email[40];
     };
     struct dados registro[6];
@@ -17,28 +17,28 @@ int main() {
     // Captura dos dados 
     for (i = 0; i < 6; i++){
             printf("Digite o %d nome\n", i+1);
-            scanf("%40[^\n]", &registro[i].nome);
+            scanf("%[^\n]", &registro[i].nome);
             // Trata a leitura indevida do \n no próximo scanf
             c = getc(stdin); 
             if (c != '\n')
                 ungetc(c, stdin);
 
             printf("Digite o %d CPF\n", i+1);
-            scanf("%11[^\n]", &registro[i].CPF);
+            scanf("%[^\n]", &registro[i].CPF);
             // Trata a leitura indevida do \n no próximo scanf
             c = getc(stdin); 
             if (c != '\n')
                 ungetc(c, stdin);
 
             printf("Digite o %d telefone\n", i+1);
-            scanf("%11[^\n]", &registro[i].telefone);
+            scanf("%[^\n]", &registro[i].telefone);
             // Trata a leitura indevida do \n no próximo scanf
             c = getc(stdin); 
             if (c != '\n')
                 ungetc(c, stdin);
 
             printf("Digite o %d email\n", i+1);
-            scanf("%40[^\n]", &registro[i].email);
+            scanf("%[^\n]", &registro[i].email);
             // Trata a leitura indevida do \n no próximo scanf
             c = getc(stdin); 
             if (c != '\n')
@@ -50,11 +50,12 @@ int main() {
 
     if(csv){
         // Faz as devidas gravações
+        fprintf(csv, "Nome;CPF;Telefone;Email;\n", registro[i].nome);
         for (i = 0; i < 6; i++){
-            fprintf(csv, "Nome;%s", i+1, registro[i].nome);
-            fprintf(csv, "CPF;%s", i+1, registro[i].CPF);
-            fprintf(csv, "Telefone;%s", i+1, registro[i].telefone);
-            fprintf(csv, "Email;%s\n", i+1, registro[i].email);
+            fprintf(csv, "%s;", registro[i].nome);
+            fprintf(csv, "%s;", registro[i].CPF);
+            fprintf(csv, "%s;", registro[i].telefone);
+            fprintf(csv, "%s;\n", registro[i].email);
         }
 
         // Fecha o arquivo
