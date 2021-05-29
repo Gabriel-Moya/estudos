7b820ac1b5a56f4eb18c6241dc44141e3f732630
Gabriel Moya <gabrielmoya123@gmail.com>
2021-05-28 23:15:34 -0400

    Aula 02 - playlist C

---
+++ estudos-em-c/playlist-youtube/aula002-funcao-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+// Caractere de escape para saltar uma linha -> \n
+
+int main(){
+
+    // printf é uma função de saída
+    printf("Ola, seja bem vindo!\n");
+
+    printf("Valor retornado: %d", printf("\nOi"));
+
+    return 0;
+}
