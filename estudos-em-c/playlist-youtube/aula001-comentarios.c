39cb04102a42cf5005949803bfb0be4f98f437db
Gabriel Moya <gabrielmoya123@gmail.com>
2021-05-28 22:54:24 -0400

    Aula 01 - playlist C

---
+++ estudos-em-c/playlist-youtube/aula001-comentarios.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    // Isso aqui é um comentário de linha única
+    /*
+        Esse é um comentário
+        de múltiplas linhas
+    */
+    printf("Hello world!\n");
+    return 0;
+}
