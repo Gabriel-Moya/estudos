4ee7fe7ca594850c4ae83a1f23cd2b999131a4c5
Gabriel Moya <gabrielmoya123@gmail.com>
2021-06-04 22:05:27 -0400

    URI #1000 python

---
+++ uri-online-judge/python/hello-world.py
@@ -0,0 +1,8 @@
+# ----------------------------------------
+# Exercício 1000 do URI
+# https://www.urionlinejudge.com.br/judge/pt/problems/view/1000
+# ----------------------------------------
+
+# -*- coding: utf-8 -*-
+
+print("Hello World!")
