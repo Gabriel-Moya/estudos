372e6c5ac46dceba74f94ec4fde62d6b47e55521
Gabriel Moya <gabrielmoya123@gmail.com>
2021-06-15 14:21:55 -0400

    Aula 06 - variáveis

---
+++ python/python3-luiz-otavio-udemy/secao02-python-basico/aula06-variaveis.py
@@ -0,0 +1,8 @@
+nome = 'Gabriel'        # String
+idade = 25              # int
+altura = 1.78           # float
+peso = 80
+e_maior = idade > 18    # bool
+imc = peso / (altura ** 2)
+
+print(nome, 'tem', idade, 'de idade e seu IMC é', imc)
