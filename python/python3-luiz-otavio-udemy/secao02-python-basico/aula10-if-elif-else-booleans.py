68c3e80f26bdfd3a98dd4195300341092ecf9fe1
Gabriel Moya <gabrielmoya123@gmail.com>
2021-06-15 15:06:04 -0400

    Aula 10 - if elif else

---
+++ python/python3-luiz-otavio-udemy/secao02-python-basico/aula10-if-elif-else-booleans.py
@@ -0,0 +1,11 @@
+if False:
+    print("Verdadeiro.")
+    print("teste2")
+elif True:
+    print("Agora é verdadeiro.")
+    nome = input("Qual o seu nome? ")
+elif False:
+    print("Esse bloco não será executado")
+    print(22 + 22)
+else:
+    print("Nenhuma opção anterior pode ter passado para executar aqui.")
