c13984ee75e5b22d0c4bcc9672b7a3e1dc85224d
Gabriel Moya <gabrielmoya123@gmail.com>
2021-06-24 16:28:46 -0400

    Aula 21 - laco for in

---
+++ python/python3-luiz-otavio-udemy/secao02-python-basico/aula21-laco-for-in.py
@@ -0,0 +1,17 @@
+texto = 'Python'
+
+for letra in texto:
+    print(letra)
+
+print()
+for n in range(10):
+    print(n)
+
+print()
+# Função range(start=0, stop, step=1)
+for n in range(1, 10, 2):
+    print(n)
+
+print()
+for n in range(20, 10, -1):
+    print(n)
