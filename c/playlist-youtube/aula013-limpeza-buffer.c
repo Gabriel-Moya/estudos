63fdbcc960fbab2c40f86cb9c3b5c6b2e0404075
Gabriel Moya <gabrielmoya123@gmail.com>
2021-06-29 23:49:31 -0400

    Aula 013 - limpeza buffer do teclado

---
+++ c/playlist-youtube/aula013-limpeza-buffer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+
+    char a, b;
+
+    printf("Digite duas letras: ");
+    scanf("%c %c", &a, &b);
+
+    printf("Primeira letra: %c\nSegunda letra: %c\n");
+
+    return 0;
+}
