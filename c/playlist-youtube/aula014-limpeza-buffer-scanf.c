1750e95bb88cc62b36bc12cae0d6e5f234ac4f2c
Gabriel Moya <gabrielmoya123@gmail.com>
2021-06-29 23:52:08 -0400

    Aula 14 - Limpeza buffer teclado com scanf

---
+++ c/playlist-youtube/aula014-limpeza-buffer-scanf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+
+    char a, b;
+
+    printf("Digite uma letra: ");
+    scanf("%c", &a);
+
+    scanf("%c");
+
+    printf("Digite uma letra: ");
+    scanf("%c", &b);
+
+    printf("Primeira letra: %c\nSegunda letra: %c\n");
+
+    return 0;
+}
