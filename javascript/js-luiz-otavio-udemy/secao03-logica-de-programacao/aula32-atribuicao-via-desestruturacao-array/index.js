95503d44bac5223eae58f4f7199c0ad82ca97a9b
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-01 23:49:37 -0400

    Aula 32 - atribuição via desestruturação arrays

---
+++ javascript/js-luiz-otavio-udemy/secao03-logica-de-programacao/aula32-atribuicao-via-desestruturacao-array/index.js
@@ -0,0 +1,15 @@
+let a = 'A'; // B
+let b = 'B'; // C
+let c = 'C'; // A
+
+const letras = [b, c, a];
+[a, b, c] = letras;
+
+console.log(a, b, c);
+
+
+//               0  1  2  3  4  5  6  7  8
+const numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+const [primeiroNumero, segundoNumero, ...resto] = numeros;
+
+console.log(primeiroNumero, segundoNumero, resto)
