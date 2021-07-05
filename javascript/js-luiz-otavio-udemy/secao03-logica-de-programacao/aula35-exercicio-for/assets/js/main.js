59909bfefb5193056cdfa191dd1ff0b582db23cb
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-04 23:24:33 -0400

    Aula 35 - exercício for

---
+++ javascript/js-luiz-otavio-udemy/secao03-logica-de-programacao/aula35-exercicio-for/assets/js/main.js
@@ -0,0 +1,18 @@
+const elementos = [
+    {tag: 'p', texto: 'Frase 1'},
+    {tag: 'div', texto: 'Frase 2'},
+    {tag: 'footer', texto: 'Frase 3'},
+    {tag: 'section', texto: 'Frase 4'}
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++){
+    let { tag, texto } = elementos[i];
+    let elemento = document.createElement(tag);
+    elemento.innerText = texto;
+    div.appendChild(elemento);
+}
+
+container.appendChild(div);
