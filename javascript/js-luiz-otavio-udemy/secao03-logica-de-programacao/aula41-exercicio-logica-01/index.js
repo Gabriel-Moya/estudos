257b48c0efb70ac8c039fa20139360aeac9c781a
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-07 21:05:50 -0400

    Aula 41 - Exercício lógica 01

---
+++ javascript/js-luiz-otavio-udemy/secao03-logica-de-programacao/aula41-exercicio-logica-01/index.js
@@ -0,0 +1,9 @@
+// Escreva uma função que recebe 2 números e retorne o maior deles
+
+function max (x, y) {
+    return x > y ? x : y;
+}
+console.log(max(10, 20));
+
+const max2 = (x, y) => x > y ? x : y;
+console.log(max2(50, 20));
