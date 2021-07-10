7aee04c89b4708fcccbfb435ce0241770b422a61
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-10 15:55:34 -0400

    Aula 021 - tipo double

---
+++ c/playlist-youtube/aula021-double.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+
+    float x = 3.1415;
+
+    double y = 3.14159265359;
+
+    printf("Um double precisa de %d bytes de memoria.\n", sizeof y);
+
+    return 0;
+}
