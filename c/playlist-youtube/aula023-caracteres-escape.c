86f6ca44dfb90d2f657923041066ecc1fd9f21b4
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-11 23:09:59 -0400

    Aula 23 - caracteres de escape

---
+++ c/playlist-youtube/aula023-caracteres-escape.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/*
+        Quebra de linha -> \n
+        Tabulação       -> \t
+        Aspa dupla      -> \"
+        Aspa simples    -> \'
+        Contrabarra     -> \\
+*/
+
+int main() {
+
+    printf("\n\n\tEssa mensagem sera impressa \'na\' \\ tela.\n\n");
+
+    return 0;
+}
