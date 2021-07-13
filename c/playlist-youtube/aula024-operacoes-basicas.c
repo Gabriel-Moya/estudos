ea84e3e8202d1aed411532425dc14a0d218d2491
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-12 22:16:07 -0400

    Aula 024 - operações básicas de matemática

---
+++ c/playlist-youtube/aula024-operacoes-basicas.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+    printf("\nSoma: %d\n", 10 + 20);
+    printf("\nSubtracao: %d\n", 10 - 20);
+    printf("\nMultiplicacao: %d\n", 10 * 20);
+    printf("\nDivisao: %.2f\n", 10 / 20);
+
+    return 0;
+}
