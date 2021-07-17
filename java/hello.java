30bd108bde7e6b96d2d693b334d7fbba82704e2b
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-16 21:40:06 -0400

    Hello java

--- java/hello.java
+++ java/hello.java
@@ -1,7 +1,7 @@
-public class Main {
+public class hello {
 
     public static void main(String[] args) {
-        System.out.Println("Hello world!");
+        System.out.println("Hello world!");
     }
 
 }
