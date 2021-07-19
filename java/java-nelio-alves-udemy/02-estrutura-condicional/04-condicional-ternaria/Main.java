7dc36cacad33fb568c5141a399f894b5e14b1dd7
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-18 21:30:21 -0400

    Condicional ternaria

---
+++ java/java-nelio-alves-udemy/02-estrutura-condicional/04-condicional-ternaria/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		double preco = 34.5;
+		double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+		
+		System.out.println(desconto);
+
+	}
+
+}
