c14b5d4c7007004386549290c1fb2ba35e552c32
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-19 23:17:02 -0400

    For

---
+++ java/java-nelio-alves-udemy/03-estrutura-repetitiva/02-for/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		for (int i = 0; i<=5; i++) {
+			System.out.println("Valor de i: " + i);
+		}
+		
+		System.out.println("########");
+		
+		for (int i = 5; i>=0; i--) {
+			System.out.println("Valor de i: " + i);
+		}
+
+	}
+
+}
