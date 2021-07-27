6a84aec85acff562402efcdc002774656b9d07da
Gabriel Moya <gabrielmoya123@gmail.com>
2021-07-26 23:35:18 -0400

    Aula 028 - operador de decremento

---
+++ c/playlist-youtube/aula028-operador-decremento.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+
+    int resultado, contador = 10;
+
+    // Sinônimas
+    contador--;
+    contador -= 1;
+    contador = contador -1;
+
+    printf("Valor: %d\n", contador++);
+    printf("Valor: %d\n", contador);
+
+    resultado = contador--;
+    printf("Valor: %d\n", resultado);
+    resultado = --contador;
+    printf("Valor: %d\n", resultado);
+
+    return 0;
+}
