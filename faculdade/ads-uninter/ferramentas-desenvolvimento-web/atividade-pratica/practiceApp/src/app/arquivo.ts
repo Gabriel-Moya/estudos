7b00428aabb10a2df1ce23024f29c6dd8bacb669
Gabriel Moya <gabrielmoya123@gmail.com>
2021-12-13 16:17:24 -0400

    Atividade pratica

---
+++ faculdade/ads-uninter/ferramentas-desenvolvimento-web/atividade-pratica/practiceApp/src/app/arquivo.ts
@@ -0,0 +1,32 @@
+export const alunos = [
+    {
+        nome: "Gabriel Marques Moya",
+        ru: 3430951,
+        curso: "Análise e Desenvolvimento de Sistemas",
+        aniversario: "15/07/1996"
+    },
+    {
+        nome: "Homer Simpson",
+        ru: 1122333,
+        curso: "Quimica",
+        aniversario: "12/05/1956"
+    },
+    {
+        nome: "Marge Simpson",
+        ru: 1122334,
+        curso: "Ciencia de Dados",
+        aniversario: "23/10/1956"
+    },
+    {
+        nome: "Bart Simpson",
+        ru: 1122335,
+        curso: "Sistemas de Informação",
+        aniversario: "28/08/1966"
+    },
+    {
+        nome: "Lisa Simpson",
+        ru: 1122336,
+        curso: "Arquitetura",
+        aniversario: "18/07/1968"
+    },
+]
