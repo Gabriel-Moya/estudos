1e2e187a360440e293021a45671db82d80f01848
Gabriel Moya <gabrielmoya123@gmail.com>
2022-01-09 22:13:06 -0400

    Update JS - New year countdown

---
+++ projects-front-end/new-year-countdown/app.js
@@ -0,0 +1,23 @@
+const secondsContainer = document.querySelector("#seconds");
+const minutesContainer = document.querySelector("#minutes");
+const hoursContainer = document.querySelector("#hours");
+const daysContainer = document.querySelector("#days");
+
+const nextYear = new Date().getFullYear() + 1;
+const newYearTIme = new Date(`January 01 ${nextYear} 00:00:00`);
+
+const updateCountdown = () => {
+  const currentTime = new Date();
+  const difference = newYearTIme - currentTime;
+  const days = Math.floor(difference / 1000 / 60 / 60 / 24);
+  const hours = Math.floor(difference / 1000 / 60 / 60) % 24;
+  const minutes = Math.floor(difference / 1000 / 60) % 60;
+  const seconds = Math.floor(difference / 1000) % 60;
+
+  secondsContainer.textContent = seconds < 10 ? "0" + seconds : seconds;
+  minutesContainer.textContent = minutes < 10 ? "0" + minutes : minutes;
+  hoursContainer.textContent = hours < 10 ? "0" + hours : hours;
+  daysContainer.textContent = days < 10 ? "0" + days : days;
+};
+
+setInterval(updateCountdown, 1000);
