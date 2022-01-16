dccb533eacdf92a42c867a68bd42cadb3f020a3e
Gabriel Moya <gabrielmoya123@gmail.com>
2022-01-16 01:27:33 -0400

    1042 - Sort Simples

---
+++ uri-online-judge/C/sort-simples.c
@@ -0,0 +1,38 @@
+/*----------------------------------------*/
+/*Exercício 1044 do URI*/
+/*https://www.urionlinejudge.com.br/judge/pt/problems/view/1044*/
+/*----------------------------------------*/
+
+#include <stdio.h>
+
+#define TAM 3
+
+int main(void) {
+
+  int a, b, c, i, j, aux;
+  int vet[TAM];
+
+  scanf("%d %d %d", &a, &b, &c);
+  vet[0] = a;
+  vet[1] = b;
+  vet[2] = c;
+
+  for (i = 0; i < TAM; i++) {
+    for (j = 0; j < TAM; j++) {
+      if (vet[i] < vet[j]) {
+        aux = vet[j];
+        vet[j] = vet[i];
+        vet[i] = aux;
+      }
+    }
+  }
+
+  for (i = 0; i < TAM; i++) {
+    printf("%d\n", vet[i]);
+  }
+
+  printf("\n");
+  printf("%d\n%d\n%d\n", a, b, c);
+
+  return 0;
+}
