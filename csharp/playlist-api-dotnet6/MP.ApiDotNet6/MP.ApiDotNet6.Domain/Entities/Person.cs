c5dd518e5e8d05882c233fdbce5fc4c699211898
Gabriel Moya <gabriel.mm@lyncas.net>
2022-07-24 17:19:29 -0300

    Aula 02 - Criando a entidade de pessoa

---
+++ csharp/playlist-api-dotnet6/MP.ApiDotNet6/MP.ApiDotNet6.Domain/Entities/Person.cs
@@ -0,0 +1,40 @@
+﻿using MP.ApiDotNet6.Domain.Validations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Domain.Entities;
+
+public sealed class Person
+{
+    public Person(string document, string name, string phone)
+    {
+        Validation(document, name, phone);
+    }
+
+    public Person(int id, string document, string name, string phone)
+    {
+        DomainValidationException.When(id < 0, "ID deve ser maior que zero!");
+        Id = id;
+
+        Validation(document, name, phone);
+    }
+
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string Document { get; private set; }
+    public string Phone { get; private set; }
+
+    private void Validation(string document, string name, string phone)
+    {
+        DomainValidationException.When(string.IsNullOrEmpty(name), "Nome deve ser informado!");
+        DomainValidationException.When(string.IsNullOrEmpty(document), "Documento deve ser informardo!");
+        DomainValidationException.When(string.IsNullOrEmpty(phone), "Celular deve ser informardo!");
+
+        Name = name;
+        Document = document;
+        Phone = phone;
+    }
+}
