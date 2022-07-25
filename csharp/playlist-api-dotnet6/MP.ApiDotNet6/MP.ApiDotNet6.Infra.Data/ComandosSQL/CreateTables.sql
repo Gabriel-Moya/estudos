7703eaf12978f9dae415709941b3c5a9583f48cf
Gabriel Moya <gabriel.mm@lyncas.net>
2022-07-24 22:46:30 -0300

    Aula 04 - Criação do banco de dados

---
+++ csharp/playlist-api-dotnet6/MP.ApiDotNet6/MP.ApiDotNet6.Infra.Data/ComandosSQL/CreateTables.sql
@@ -0,0 +1,25 @@
+﻿--Create table PESSOAS
+create table if not exists PESSOA(
+	IDPESSOA SERIAL primary key,
+	NOME VARCHAR(100),
+	DOCUMENTO VARCHAR(20),
+	CELULAR VARCHAR(20)
+);
+
+--Create table PRODUTOS
+create table if not exists PRODUTO(
+	IDPRODUTO SERIAL primary key,
+	NOME VARCHAR(100),
+	CODERP VARCHAR(10),
+	PRECO NUMERIC(10,2)
+);
+
+--Create table COMPRAS
+create table if not exists COMPRA(
+	IDCOMPRA SERIAL primary key,
+	IDPRODUTO INT,
+	IDPESSOA INT,
+	DATACOMPRA DATE,
+	constraint FK_PESSOA_COMPRA foreign KEY(IDPESSOA) references PESSOA(IDPESSOA),
+	constraint FK_PRODUTO_COMPRA foreign key(IDPRODUTO) references PRODUTO(IDPRODUTO)
+);
