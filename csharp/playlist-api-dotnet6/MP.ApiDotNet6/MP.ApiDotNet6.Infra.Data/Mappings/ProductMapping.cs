06f12bbba9607adee4dffeab8af9a688688c3d1c
Gabriel Moya <gabriel.mm@lyncas.net>
2022-07-25 22:12:41 -0300

    Aula 06 - Mapeando das classes produtos e compras

---
+++ csharp/playlist-api-dotnet6/MP.ApiDotNet6/MP.ApiDotNet6.Infra.Data/Mappings/ProductMapping.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MP.ApiDotNet6.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Infra.Data.Mappings;
+
+public class ProductMapping : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.ToTable("Produto");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("IdProduto")
+            .UseIdentityColumn();
+
+        builder.Property(x => x.CodErp)
+            .HasColumnName("CodErp");
+
+        builder.Property(x => x.Name)
+            .HasColumnName("Nome");
+
+        builder.Property(x => x.Price)
+            .HasColumnName("Preco");
+
+        builder.HasMany(x => x.Purchases)
+            .WithOne(x => x.Product)
+            .HasForeignKey(x => x.ProductId);
+    }
+}
---
+++ csharp/playlist-api-dotnet6/MP.ApiDotNet6/MP.ApiDotNet6.Infra.Data/Mappings/PurchaseMapping.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MP.ApiDotNet6.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Infra.Data.Mappings;
+
+public class PurchaseMapping : IEntityTypeConfiguration<Purchase>
+{
+    public void Configure(EntityTypeBuilder<Purchase> builder)
+    {
+        builder.ToTable("Compra");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("IdCompra")
+            .UseIdentityColumn();
+
+        builder.Property(x => x.PersonId)
+            .HasColumnName("IdPessoa");
+
+        builder.Property(x => x.ProductId)
+            .HasColumnName("IdProduto");
+
+        builder.Property(x => x.Date)
+            .HasColumnName("DataCompra");
+
+        builder.HasOne(x => x.Person)
+            .WithMany(x => x.Purchases);
+
+        builder.HasOne(x => x.Product)
+            .WithMany(x => x.Purchases);
+    }
+}
