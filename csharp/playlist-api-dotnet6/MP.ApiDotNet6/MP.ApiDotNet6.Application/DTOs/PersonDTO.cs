191c8f676ae9ca1f59a8d9b9d4d5d61f4e668034
Gabriel Moya <gabriel.mm@lyncas.net>
2022-07-27 23:28:01 -0300

    Aula 08 - Criando DTO de pessoa e o validator

---
+++ csharp/playlist-api-dotnet6/MP.ApiDotNet6/MP.ApiDotNet6.Application/DTOs/PersonDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Application.DTOs;
+
+public class PersonDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Documento { get; set; }
+    public string Phone { get; set; }
+}
---
+++ csharp/playlist-api-dotnet6/MP.ApiDotNet6/MP.ApiDotNet6.Application/DTOs/Validators/PersonDTOValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MP.ApiDotNet6.Application.DTOs.Validators;
+
+public class PersonDTOValidator : AbstractValidator<PersonDTO>
+{
+    public PersonDTOValidator()
+    {
+        RuleFor(x => x.Documento)
+            .NotEmpty()
+            .WithMessage("Documento deve ser informado!");
+
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Nome deve ser informado!");
+
+        RuleFor(x => x.Phone)
+            .NotEmpty()
+            .WithMessage("Telefone deve ser informado!");
+    }
+}
