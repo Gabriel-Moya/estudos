8cc3344bfa8e248c4f7f15d50f3a35b9c11dcf49
Gabriel Moya <gabriel.mm@lyncas.net>
2022-08-13 23:50:10 -0300

    Mão na massa: Editor HTML

---
+++ csharp/balta-io/EditorHTML/EditorHTML/Menu.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EditorHTML
+{
+    public static class Menu
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            DrawScreen();
+            DisplayOptions();
+
+            var option = short.Parse(Console.ReadLine());
+            HandleMenuOption(option);
+        }
+
+        public static void DrawScreen()
+        {
+            DrawLines();
+            DrawColumns();
+            DrawLines();
+        }
+
+        public static void DrawLines()
+        {
+            Console.Write("+");
+
+            for (int i = 0; i <= 30; i++)
+                Console.Write("-");
+
+            Console.WriteLine("+");
+        }
+
+        public static void DrawColumns()
+        {
+            for (int lines = 0; lines <= 10; lines++)
+            {
+                Console.Write("|");
+                for (int i = 0; i <= 30; i++)
+                    Console.Write(" ");
+
+                Console.WriteLine("|");
+            }
+        }
+    
+        public static void DisplayOptions()
+        {
+            Console.SetCursorPosition(10, 2);
+            Console.WriteLine("Editor HTML");
+            Console.SetCursorPosition(3, 3);
+            Console.WriteLine("==========================");
+            Console.SetCursorPosition(3, 4);
+            Console.WriteLine("Selecione uma opção abaixo");
+            Console.SetCursorPosition(3, 6);
+            Console.WriteLine("1 - Novo arquivo");
+            Console.SetCursorPosition(3, 7);
+            Console.WriteLine("2 - Abrir");
+            Console.SetCursorPosition(3, 8);
+            Console.WriteLine("0 - Sair");
+            Console.SetCursorPosition(3, 10);
+            Console.Write("Opção: ");
+        }
+    
+        public static void HandleMenuOption(short option)
+        {
+            switch (option)
+            {
+                case 1: Editor.Show(); break;
+                case 2: Console.WriteLine("View"); break;
+                case 0:
+                    {
+                        Console.Clear();
+                        Environment.Exit(0);
+                        break;
+                    }
+                default: Show(); break;
+            }
+        }
+    }
+}
---
+++ csharp/balta-io/EditorHTML/EditorHTML/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EditorHTML
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu.Show();
+        }
+    }
+}
