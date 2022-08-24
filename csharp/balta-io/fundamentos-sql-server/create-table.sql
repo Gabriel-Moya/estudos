0f5b9e6df95f7c9dc20782b1a619b3cc4c5e3d17
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-24 20:04:53 -0300

    Constraint - Not null

--- csharp/balta-io/fundamentos-sql-server/create-table.sql
+++ csharp/balta-io/fundamentos-sql-server/create-table.sql
@@ -1,9 +1,12 @@
 USE [CursoBalta]
 
 CREATE TABLE [Aluno](
-    [Id] INT,
-    [Nome] NVARCHAR(80),
-    [Nascimento] DATETIME,
-    [Active] BIT
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Nascimento] DATETIME NULL,
+    [Active] BIT DEFAULT(0)
 )
 GO
+
+ALTER TABLE [Aluno]
+    ALTER COLUMN [Active] BIT NOT NULL
