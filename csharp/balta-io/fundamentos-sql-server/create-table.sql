1830ab78f8578c185ade78c21988b29bef2331a9
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-24 22:50:37 -0300

    Composite key and Foreign key

--- csharp/balta-io/fundamentos-sql-server/create-table.sql
+++ csharp/balta-io/fundamentos-sql-server/create-table.sql
@@ -1,5 +1,6 @@
 USE [CursoBalta]
 
+DROP TABLE [Aluno]
 CREATE TABLE [Aluno](
     [Id] INT NOT NULL,
     [Nome] NVARCHAR(80) NOT NULL,
@@ -12,8 +13,34 @@ CREATE TABLE [Aluno](
 )
 GO
 
-ALTER TABLE [Aluno]
-    ALTER COLUMN [Active] BIT NOT NULL
+DROP TABLE [Curso]
+CREATE TABLE [Curso](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [CategoriaId] INT NOT NULL,
 
-ALTER TABLE [Aluno]
-    DROP CONSTRAINT [PK_Aluno]
+    CONSTRAINT [PK_Curso] PRIMARY KEY([Id]),
+    CONSTRAINT [FK_Curso_Categoria] FOREIGN KEY([CategoriaId])
+        REFERENCES [Categoria]([Id])
+)
+GO
+
+DROP TABLE [ProgressoCurso]
+CREATE TABLE [ProgressoCurso](
+    [AlunoId] INT NOT NULL,
+    [CursoId] INT NOT NULL,
+    [Progresso] INT NOT NULL,
+    [UltimaAtualizacao] DATETIME NOT NULL DEFAULT(GETDATE()),
+    
+    CONSTRAINT [PK_ProgressoCurso] PRIMARY KEY([AlunoId], [CursoId])
+)
+GO
+
+DROP TABLE [Categoria]
+CREATE TABLE [Categoria](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+
+    CONSTRAINT [PK_Categoria] PRIMARY KEY([Id]),
+)
+GO
