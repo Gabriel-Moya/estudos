cc165bb0ad7ac6a4db62881fc3a408609fede080
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-24 22:33:39 -0300

    Primary key

--- csharp/balta-io/fundamentos-sql-server/create-table.sql
+++ csharp/balta-io/fundamentos-sql-server/create-table.sql
@@ -1,13 +1,19 @@
 USE [CursoBalta]
 
 CREATE TABLE [Aluno](
-    [Id] INT NOT NULL UNIQUE,
+    [Id] INT NOT NULL,
     [Nome] NVARCHAR(80) NOT NULL,
-    [Email] NVARCHAR(180) NOT NULL UNIQUE,
+    [Email] NVARCHAR(180) NOT NULL,
     [Nascimento] DATETIME NULL,
-    [Active] BIT DEFAULT(0)
+    [Active] BIT DEFAULT(0),
+
+    CONSTRAINT [PK_Aluno] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_Aluno_Email] UNIQUE([Email]),
 )
 GO
 
 ALTER TABLE [Aluno]
     ALTER COLUMN [Active] BIT NOT NULL
+
+ALTER TABLE [Aluno]
+    DROP CONSTRAINT [PK_Aluno]
