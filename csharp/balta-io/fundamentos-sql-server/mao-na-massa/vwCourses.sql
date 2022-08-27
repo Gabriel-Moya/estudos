757805d1ea30d123f95542b624e16c84532e0d2d
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 17:24:30 -0400

    Listing the courses

---
+++ csharp/balta-io/fundamentos-sql-server/mao-na-massa/vwCourses.sql
@@ -0,0 +1,22 @@
+USE [balta]
+GO
+
+CREATE OR ALTER VIEW vwCourses AS
+    SELECT
+        [Course].[Id],
+        [Course].[Tag],
+        [Course].[Title],
+        [Course].[Url],
+        [Course].[Summary],
+        [Course].[CreateDate],
+        [Category].[Title] AS [Category],
+        [Author].[Name] AS [Author]
+    FROM
+        [Course]
+        INNER JOIN [Category] ON [Course].[CategoryId] = [Category].[Id]
+        INNER JOIN [Author] ON [Course].[AuthorId] = [Author].[Id]
+    WHERE
+        [Active] = 1
+GO
+
+SELECT * FROM vwCourses ORDER BY [CreateDate] DESC
