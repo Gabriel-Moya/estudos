2d8a6919dfbc85a728e3cb111d5b23ff05f99386
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 11:15:08 -0400

    Min, max, count

---
+++ csharp/balta-io/fundamentos-sql-server/min-max-count.sql
@@ -0,0 +1,22 @@
+SELECT TOP 100
+    MIN([ID])
+FROM
+    [Categoria]
+
+
+SELECT TOP 100
+    MAX([ID])
+FROM
+    [Categoria]
+
+
+SELECT TOP 100
+    AVG([ID])
+FROM
+    [Categoria]
+
+
+SELECT TOP 100
+    COUNT([ID])
+FROM
+    [Categoria]
