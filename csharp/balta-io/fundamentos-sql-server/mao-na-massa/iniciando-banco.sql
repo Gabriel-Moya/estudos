1029c794ca52fe39f4e1db1c1ea86af4b16fd273
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 16:16:21 -0400

    Creating the tables - part 2

--- csharp/balta-io/fundamentos-sql-server/mao-na-massa/iniciando-banco.sql
+++ csharp/balta-io/fundamentos-sql-server/mao-na-massa/iniciando-banco.sql
@@ -79,3 +79,31 @@ CREATE TABLE [Course]
     CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id])
 );
 GO
+
+CREATE TABLE [CareerItem]
+(
+    [CareerId] uniqueidentifier NOT NULL,
+    [CourseId] uniqueidentifier NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Order] TINYINT NOT NULL,
+    
+    CONSTRAINT [PK_CareerItem] PRIMARY KEY ([CourseId], [CareerId]),
+    CONSTRAINT [FK_CareerItem_Career_CareerId] FOREIGN KEY ([CareerId]) REFERENCES [Career] ([Id]),
+    CONSTRAINT [FK_CareerItem_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id])
+);
+GO
+
+CREATE TABLE [StudentCourse]
+(
+    [CourseId] uniqueidentifier NOT NULL,
+    [StudentId] uniqueidentifier NOT NULL,
+    [Progress] TINYINT NOT NULL,
+    [Favorite] BIT NOT NULL,
+    [StartDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NULL DEFAULT(GETDATE()),
+    CONSTRAINT [PK_StudentCourse] PRIMARY KEY ([CourseId], [StudentId]),
+    CONSTRAINT [FK_StudentCourse_Course_CourseId] FOREIGN KEY ([CourseId]) REFERENCES [Course] ([Id]),
+    CONSTRAINT [FK_StudentCourse_Student_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Student] ([Id])
+);
+GO
