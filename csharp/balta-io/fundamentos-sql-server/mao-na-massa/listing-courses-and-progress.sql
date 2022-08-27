d2a579282f287828c947893a26a1b5c4db56cbf1
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 19:15:56 -0400

    Listing courses and progress

---
+++ csharp/balta-io/fundamentos-sql-server/mao-na-massa/listing-courses-and-progress.sql
@@ -0,0 +1,9 @@
+SELECT
+    [Course].[Title] AS [Course],
+    [Student].[Name] AS [Student],
+    [StudentCourse].[Progress],
+    [StudentCourse].[LastUpdateDate]
+FROM
+    [Course]
+    LEFT JOIN [StudentCourse] ON [StudentCourse].[CourseId] = [Course].[Id]
+    LEFT JOIN [Student] ON [StudentCourse].[StudentId] = [Student].[Id]
