261258dab2757405da2244281cf109e3e6a6a8fe
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 12:56:55 -0400

    Group by

---
+++ csharp/balta-io/fundamentos-sql-server/group-by.sql
@@ -0,0 +1,11 @@
+SELECT TOP 100
+    [Categoria].[Id],
+    [Categoria].[Nome],
+    COUNT([Curso].[CategoriaId]) AS [Cursos]
+FROM
+    [Categoria]
+    INNER JOIN [Curso] ON [Curso].[CategoriaId] = [Categoria].[Id]
+GROUP BY
+    [Categoria].[Id],
+    [Categoria].[Nome],
+    [Curso].[CategoriaId]
