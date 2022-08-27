70eb878af8d3eed1c8f76ad360cd595922cb0125
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 12:05:02 -0400

    Inner join

---
+++ csharp/balta-io/fundamentos-sql-server/inner-join.sql
@@ -0,0 +1,9 @@
+SELECT TOP 100
+    [Curso].[Id],
+    [Curso].[Nome],
+    [Categoria].[Id] AS [Categoria],
+    [Categoria].[Nome]
+FROM
+    [Curso]
+    INNER JOIN [Categoria]
+        ON [Curso].[CategoriaId] = [Categoria].[Id]
