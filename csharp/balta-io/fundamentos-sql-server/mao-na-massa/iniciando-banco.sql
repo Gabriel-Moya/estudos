aa77a01635ee7775c37ead22ff56378ac75a5487
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 16:09:32 -0400

    Creating the tables - part 1

--- csharp/balta-io/fundamentos-sql-server/mao-na-massa/iniciando-banco.sql
+++ csharp/balta-io/fundamentos-sql-server/mao-na-massa/iniciando-banco.sql
@@ -16,3 +16,66 @@ CREATE TABLE [Student]
     CONSTRAINT [PK_Student] PRIMARY KEY ([Id])
 );
 GO
+
+CREATE TABLE [Author]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Name] NVARCHAR(80) NOT NULL,
+    [Title] NVARCHAR(80) NOT NULL,
+    [Image] NVARCHAR(1024) NOT NULL,
+    [Bio] NVARCHAR(2000) NOT NULL,
+    [URL] NVARCHAR(450) NULL,
+    [Email] NVARCHAR(160) NOT NULL,
+    [Type] TINYINT NOT NULL,
+    CONSTRAINT [PK_Author] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Career]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(60) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [URL] NVARCHAR(1024) NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [Active] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Career] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Category]
+(
+    [Id] UNIQUEIDENTIFIER NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [URL] NVARCHAR(1024) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Description] TEXT NOT NULL,
+    [Featured] BIT NOT NULL,
+    CONSTRAINT [PK_Category] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Course]
+(
+    [Id] uniqueidentifier NOT NULL,
+    [Tag] NVARCHAR(20) NOT NULL,
+    [Title] NVARCHAR(160) NOT NULL,
+    [Summary] NVARCHAR(2000) NOT NULL,
+    [Url] NVARCHAR(1024) NOT NULL,
+    [Level] TINYINT NOT NULL,
+    [DurationInMinutes] INT NOT NULL,
+    [CreateDate] DATETIME NOT NULL,
+    [LastUpdateDate] DATETIME NOT NULL,
+    [Active] BIT NOT NULL,
+    [Free] BIT NOT NULL,
+    [Featured] BIT NOT NULL,
+    [AuthorId] uniqueidentifier NOT NULL,
+    [CategoryId] uniqueidentifier NOT NULL,
+    [Tags] NVARCHAR(160) NOT NULL,
+    CONSTRAINT [PK_Course] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Course_Author_AuthorId] FOREIGN KEY ([AuthorId]) REFERENCES [Author] ([Id]),
+    CONSTRAINT [FK_Course_Category_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id])
+);
+GO
