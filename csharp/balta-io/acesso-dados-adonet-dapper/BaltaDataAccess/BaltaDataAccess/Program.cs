6a4d18d32869d1357eb342671b962d1622a7bdd7
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-27 23:30:25 -0400

    Connection string

---
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BaltaDataAccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=senhaS3creta";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
