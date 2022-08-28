a9b5d3c68f9bad55698f815f148be531f766c76b
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-28 15:05:13 -0400

    Update

--- csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
@@ -11,6 +11,27 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=senhaS3creta;TrustServerCertificate=True";
 
+            
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                UpdateCategory(connection);
+                ListCategories(connection);
+                //CreateCategory(connection);
+            }
+        }
+
+        static void ListCategories(SqlConnection connection)
+        {
+            var categories = connection.Query<Category>("SELECT [ID], [Title] FROM [Category]");
+            foreach (var item in categories)
+            {
+                Console.WriteLine($"{item.Id} - {item.Title}");
+            }
+        }
+
+        static void CreateCategory(SqlConnection connection)
+        {
             var category = new Category();
             category.Id = Guid.NewGuid();
             category.Title = "Amazon AWS";
@@ -31,26 +52,29 @@ namespace BaltaDataAccess
                                 @Description,
                                 @Featured)";
 
-            using (var connection = new SqlConnection(connectionString))
+            var rows = connection.Execute(insertSql, new
             {
-                var rows = connection.Execute(insertSql, new
-                {
-                    category.Id,
-                    category.Title,
-                    category.Url,
-                    category.Summary,
-                    category.Order,
-                    category.Description,
-                    category.Featured
-                });
-                Console.WriteLine($"{rows} linhas inseridas");
+                category.Id,
+                category.Title,
+                category.Url,
+                category.Summary,
+                category.Order,
+                category.Description,
+                category.Featured
+            });
+            Console.WriteLine($"{rows} linhas inseridas");
+        }
+    
+        static void UpdateCategory(SqlConnection connection)
+        {
+            var updateQuery = "UPDATE [Category] SET [Title]=@Title WHERE [Id] = @Id";
+            var rows = connection.Execute(updateQuery, new
+            {
+                Id = new Guid("af3407aa-11ae-4621-a2ef-2028b85507c4"),
+                Title = "Frontend 2022",
+            });
 
-                var categories = connection.Query<Category>("SELECT [ID], [Title] FROM [Category]");
-                foreach (var item in categories)
-                {
-                    Console.WriteLine($"{item.Id} - {item.Title}");
-                }
-            }
+            Console.WriteLine($"{rows} registros atualizados");
         }
     }
 }
