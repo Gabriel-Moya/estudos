31af17212e5e416acd15defe05e0b65139b9dd65
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-28 00:25:47 -0400

    SqlCommand and SqlDataReader

--- csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Data.SqlClient;
 using System;
+using System.Data;
 
 namespace BaltaDataAccess
 {
@@ -7,11 +8,25 @@ namespace BaltaDataAccess
     {
         static void Main(string[] args)
         {
-            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=senhaS3creta";
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=senhaS3creta;TrustServerCertificate=True";
 
             using (var connection = new SqlConnection(connectionString))
             {
                 Console.WriteLine("Conexão realizada!");
+                connection.Open();
+                
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
             }
             Console.WriteLine("Conexão finalizada!");
 
