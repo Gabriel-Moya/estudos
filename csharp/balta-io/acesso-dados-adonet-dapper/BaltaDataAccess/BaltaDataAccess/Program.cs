e6c6b4b34845e69f81e5a2e8f8826d211c2cdb77
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-28 00:29:50 -0400

    Add Dapper

--- csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
@@ -12,23 +12,8 @@ namespace BaltaDataAccess
 
             using (var connection = new SqlConnection(connectionString))
             {
-                Console.WriteLine("Conexão realizada!");
-                connection.Open();
                 
-                using (var command = new SqlCommand())
-                {
-                    command.Connection = connection;
-                    command.CommandType = CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-
-                    var reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
-                    }
-                }
             }
-            Console.WriteLine("Conexão finalizada!");
         }
     }
 }
