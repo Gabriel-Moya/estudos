db8da4ac653567aa94d58066f696be86bb5495e1
Gabriel Moya <gabrielmoya123@gmail.com>
2022-08-28 14:46:21 -0400

    SQL Injection

--- csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
@@ -2,8 +2,6 @@
 using Dapper;
 using Microsoft.Data.SqlClient;
 using System;
-using System.Collections.Generic;
-using System.Data;
 
 namespace BaltaDataAccess
 {
@@ -14,7 +12,25 @@ namespace BaltaDataAccess
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=senhaS3creta;TrustServerCertificate=True";
 
             var category = new Category();
-            var insertSql = "INSERT INTO [Category] VALUES(NEWID(), title, url, summary, order, featured)";
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços da AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            // Previnindo SQL Injection
+
+            var insertSql = @"INSERT INTO
+                                [Category]
+                            VALUES(
+                                NEWID(),
+                                title,
+                                url,
+                                summary,
+                                order,
+                                featured)";
 
             using (var connection = new SqlConnection(connectionString))
             {
