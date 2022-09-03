8aea92f41801d5b3dac8ec1057a2c9b851a3fa24
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-03 11:12:24 -0400

    Creating the database

---
+++ csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/blog.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE [Blog]
+GO
+
+USE [Blog]
+GO
+
+-- DROP TABLE [User]
+-- DROP TABLE [Role]
+-- DROP TABLE [UserRole]
+-- DROP TABLE [Post]
+-- DROP TABLE [Category]
+-- DROP TABLE [Tag]
+-- DROP TABLE [PostTag]
+
+CREATE TABLE [User] (
+    [Id] INT NOT NULL IDENTITY(1, 1),
+    [Name] NVARCHAR(80) NOT NULL,
+    [Email] VARCHAR(200) NOT NULL,
+    [PasswordHash] VARCHAR(255) NOT NULL,
+    [Bio] TEXT NOT NULL,
+    [Image] VARCHAR(2000) NOT NULL,
+    [Slug] VARCHAR(80) NOT NULL,
+
+    CONSTRAINT [PK_User] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_User_Email] UNIQUE([Email]),
+    CONSTRAINT [UQ_User_Slug] UNIQUE([Slug])
+)
+CREATE NONCLUSTERED INDEX [IX_User_Email] ON [User]([Email])
+CREATE NONCLUSTERED INDEX [IX_User_Slug] ON [User]([Slug])
