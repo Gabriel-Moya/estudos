afc63511b555cb645c9215e67a6caec859798001
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-03 11:30:03 -0300

    Transactions

--- csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
@@ -29,7 +29,8 @@ namespace BaltaDataAccess
                 //OneToMany(connection);
                 //QueryMultiple(connection);
                 //SelectIn(connection);
-                Like(connection, "criando");
+                //Like(connection, "criando");
+                Transaction(connection);
 
             }
         }
@@ -216,7 +217,7 @@ namespace BaltaDataAccess
                 Console.WriteLine($"{item.Id} - {item.Title}");
             }
         }
-    
+
         static void OneToOne(SqlConnection connection)
         {
             var sql = @"
@@ -344,6 +345,49 @@ namespace BaltaDataAccess
             foreach (var item in items)
             {
                 Console.WriteLine(item.Title);
+            }
+        }
+
+        static void Transaction(SqlConnection connection)
+        {
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Categoria salva";
+            category.Url = "amazon";
+            category.Description = "Categoria destinada a serviços da AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+
+            var insertSql = @"INSERT INTO
+                                [Category]
+                            VALUES(
+                                @Id,
+                                @Title,
+                                @Url,
+                                @Summary,
+                                @Order,
+                                @Description,
+                                @Featured)";
+
+            connection.Open();
+            using (var transaction = connection.BeginTransaction())
+            {
+                var rows = connection.Execute(insertSql, new
+                {
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                }, transaction);
+
+                transaction.Commit();
+                //transaction.Rollback();
+
+                Console.WriteLine($"{rows} linhas inseridas");
             }
         }
     }
