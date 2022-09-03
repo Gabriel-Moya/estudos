20fefa72d788d23c735a0325917280058d933e0a
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-03 10:43:29 -0300

    OneToMany - Part 1

---
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Models/Career.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaltaDataAccess.Models
+{
+    public class Career
+    {
+        public Career()
+        {
+            Items = new List<CareerItem>();
+        }
+
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public IList<CareerItem> Items { get; set; }
+    }
+}
--- csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/BaltaDataAccess/BaltaDataAccess/Program.cs
@@ -23,7 +23,8 @@ namespace BaltaDataAccess
                 //ExecuteReadProcedure(connection);
                 //ExecuteScalar(connection);
                 //ReadView(connection);
-                OneToOne(connection);
+                //OneToOne(connection);
+                OneToMany(connection);
             }
         }
 
@@ -231,6 +232,38 @@ namespace BaltaDataAccess
             foreach (var item in items)
             {
                 Console.WriteLine($"{item.Title} - Curso: {item.Course.Title}");
+            }
+        }
+
+        static void OneToMany(SqlConnection connection)
+        {
+            var sql = @"
+                SELECT
+                    [Career].[Id],
+                    [Career].[Title],
+                    [CareerItem].[CareerId],
+                    [CareerItem].[Title]
+                FROM
+                    [Career]
+                INNER JOIN
+                    [CareerItem] ON [CareerItem].[CareerId] = [Career].[Id]
+                ORDER BY
+                    [Career].[Title]";
+
+            var careers = connection.Query<Career, CareerItem, Career>(
+                sql,
+                (career, item) =>
+                {
+                    return career;
+                }, splitOn: "CareerId");
+
+            foreach (var career in careers)
+            {
+                Console.WriteLine($"{career.Title}");
+                foreach (var item in career.Items)
+                {
+                    Console.WriteLine($" - {item.Title}");
+                }
             }
         }
     }
