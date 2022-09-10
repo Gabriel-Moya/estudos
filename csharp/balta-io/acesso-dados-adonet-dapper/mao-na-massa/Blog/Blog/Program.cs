9bf49dd100bf25b4e7d70945e45f5ac495fd0ba0
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-10 11:16:55 -0400

    CRUD

--- csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Program.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Program.cs
@@ -11,6 +11,13 @@ namespace Blog
 
         static void Main(string[] args)
         {
+            //ReadUsers();
+            //ReadUser();
+            //CreateUser();
+            //ReadUsers();
+            //UpdateUser();
+            ReadUsers();
+            DeleteteUser();
             ReadUsers();
         }
 
@@ -24,6 +31,64 @@ namespace Blog
                 {
                     Console.WriteLine(user.Name);
                 }
+            }
+        }
+
+        public static void ReadUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var user = connection.Get<User>(1);
+                Console.WriteLine(user.Name);
+            }
+        }
+
+        public static void CreateUser()
+        {
+            var user = new User()
+            {
+                Bio = "Equipe balta.io",
+                Email = "hello@balta.io",
+                Image = "https://",
+                Name = "Equipe balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Insert<User>(user);
+                Console.WriteLine("Cadastro realizado com sucesso");
+            }
+        }
+
+        public static void UpdateUser()
+        {
+            var user = new User()
+            {
+                Id = 2,
+                Bio = "Equipe | balta.io",
+                Email = "hello@balta.io",
+                Image = "https://",
+                Name = "Equipe de suporte balta.io",
+                PasswordHash = "HASH",
+                Slug = "equipe-balta"
+            };
+
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                connection.Update<User>(user);
+                Console.WriteLine("Atualização realizada com sucesso");
+            }
+        }
+
+        public static void DeleteteUser()
+        {
+            using (var connection = new SqlConnection(CONNECTION_STRING))
+            {
+               var user = connection.Get<User>(2);
+                connection.Delete<User>(user);
+                Console.WriteLine("Exclusão realizada com sucesso");
             }
         }
     }
