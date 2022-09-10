e7f5240359dad75771afd4c8d537c4a2e64317b0
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-10 13:21:31 -0400

    Optimizing the code (RoleRepository)

--- csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Repositories/RoleRepository.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Repositories/RoleRepository.cs
@@ -20,5 +20,26 @@ namespace Blog.Repositories
 
         public void Create(Role role)
             => _connection.Insert<Role>(role);
+
+        public void Update(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Update<Role>(role);
+        }
+
+        public void Delete(Role role)
+        {
+            if (role.Id != 0)
+                _connection.Delete<Role>(role);
+        }
+
+        public void Delete(int id)
+        {
+            if (id != 0)
+                return;
+
+            var role = _connection.Get<Role>(id);
+            _connection.Delete<Role>(role);
+        }
     }
 }
--- csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Repositories/UserRepository.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Repositories/UserRepository.cs
@@ -19,6 +19,30 @@ namespace Blog.Repositories
             => _connection.Get<User>(id);
 
         public void Create(User user)
-            => _connection.Insert<User>(user);
+        {
+            user.Id = 0;
+            _connection.Insert<User>(user);
+        }
+
+        public void Update(User user)
+        {
+            if(user.Id != 0)
+                _connection.Update<User>(user);
+        }
+
+        public void Delete(User user)
+        {
+            if (user.Id != 0)
+                _connection.Delete<User>(user);
+        }
+
+        public void Delete(int id)
+        {
+            if (id != 0)
+                return;
+
+            var user = _connection.Get<User>(id);
+            _connection.Delete<User>(user);
+        }
     }
 }
