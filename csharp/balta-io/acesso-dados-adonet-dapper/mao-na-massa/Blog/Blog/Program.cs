56b44768488f1e798c06e1425da6e524df528549
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-10 10:52:24 -0400

    Creating a project

---
+++ csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Program.cs
@@ -0,0 +1,30 @@
+﻿using Blog.Models;
+using Dapper.Contrib.Extensions;
+using Microsoft.Data.SqlClient;
+using System;
+
+namespace Blog
+{
+    public class Program
+    {
+        private const string CONNECTION_STRING = @"Server=localhost,1433;Database=Blog;User ID=sa;Password=senhaS3creta";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public static void ReadUsers()
+        {
+            using(var connection = new SqlConnection(CONNECTION_STRING))
+            {
+                var users = connection.GetAll<User>();
+
+                foreach(var user in users)
+                {
+                    Console.WriteLine(user.Name);
+                }
+            }
+        }
+    }
+}
