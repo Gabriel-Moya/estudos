4fe20f7752190d79acf36830892e9735ec2bb6e2
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-11 19:14:06 -0400

    Modules and components

--- javascript/balta-io/comecando-com-angular/todo/src/app/app.component.ts
+++ javascript/balta-io/comecando-com-angular/todo/src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // <app-root>
   templateUrl: './app.component.html',
+  //template: '<p>Meu template</p>',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
