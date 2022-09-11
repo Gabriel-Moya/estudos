744a0e4d40f52017949f3e6a665c6da8d80e1893
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-11 13:35:37 -0400

    Fix model category

--- csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Models/Category.cs
+++ csharp/balta-io/acesso-dados-adonet-dapper/mao-na-massa/Blog/Blog/Models/Category.cs
@@ -6,6 +6,11 @@ namespace Blog.Models
     [Table("[Category]")]
     public class Category
     {
+        public Category()
+        {
+            Posts = new List<Post>();
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
