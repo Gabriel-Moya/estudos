2fcc97116a4a4ce6de481d0a37865ecc2820133a
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-18 16:59:00 -0400

    Saving the items

--- javascript/balta-io/comecando-com-angular/todo/src/app/app.component.ts
+++ javascript/balta-io/comecando-com-angular/todo/src/app/app.component.ts
@@ -28,6 +28,7 @@ export class AppComponent {
     const title = this.form.controls['title'].value;
     const id = this.todos.length + 1;
     this.todos.push(new Todo(id, title, false));
+    this.save();
     this.clear();
   }
 
@@ -52,5 +53,10 @@ export class AppComponent {
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+  }
+
+  save() {
+    const data = JSON.stringify(this.todos);
+    localStorage.setItem('todos', data)
   }
 }
