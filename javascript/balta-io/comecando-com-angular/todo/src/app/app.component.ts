f8313e810a19c7799fd449a2fb6e9e4c23e7a9af
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-18 17:05:50 -0400

    Reading the items

--- javascript/balta-io/comecando-com-angular/todo/src/app/app.component.ts
+++ javascript/balta-io/comecando-com-angular/todo/src/app/app.component.ts
@@ -21,6 +21,8 @@ export class AppComponent {
         Validators.required,
       ])]
     });
+
+    this.load();
   }
 
   add() {
@@ -45,18 +47,26 @@ export class AppComponent {
     if(index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
   markAsDone(todo: Todo) {
     todo.done = true;
+    this.save();
   }
 
   markAsUndone(todo: Todo) {
     todo.done = false;
+    this.save();
   }
 
   save() {
     const data = JSON.stringify(this.todos);
     localStorage.setItem('todos', data)
+  }
+
+  load() {
+    const data = localStorage.getItem('todos')!;
+    this.todos = JSON.parse(data);
   }
 }
