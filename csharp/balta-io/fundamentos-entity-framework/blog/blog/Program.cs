dfb886900bbb748769a57db1cab31c73daeb1752
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-24 17:18:59 -0400

    ToList

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
@@ -11,17 +11,34 @@ namespace blog
             using (var context = new BlogDataContext())
             {
                 // CREATE
-                //var tag = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                //var tag = new Tag { Name = ".NET", Slug = "dotnet" };
                 //context.Tags.Add(tag);
                 //context.SaveChanges();
 
-                // READ
-                var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-                tag.Name = ".NET";
-                tag.Slug = "dotnet";
+                //var tag2 = new Tag { Name = "ASP.NET", Slug = "aspnet" };
+                //context.Tags.Add(tag2);
+                //context.SaveChanges();
 
-                context.Update(tag);
-                context.SaveChanges();
+                // Update
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+                //tag.Name = ".NET";
+                //tag.Slug = "dotnet";
+
+                //context.Update(tag);
+                //context.SaveChanges();
+
+                // DELETE
+                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
+
+                //context.Remove(tag);
+                //context.SaveChanges();
+
+                var tags = context.Tags.ToList();
+
+                foreach (var tag in tags)
+                {
+                    System.Console.WriteLine(tag.Name);
+                }
             }
         }
     }
