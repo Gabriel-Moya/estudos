3c99b746a6f6a8b9abe0fa29b56d0be7016a5cae
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-24 14:56:47 -0400

    DbSet

---
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Data/BlogDataContext.cs
@@ -0,0 +1,11 @@
+﻿using blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
