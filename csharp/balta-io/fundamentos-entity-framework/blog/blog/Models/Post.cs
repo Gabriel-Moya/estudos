47a0966eab7e2c0dfe5d846bca89b15d55af030f
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-24 23:23:58 -0400

    Navigation property

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/Post.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/Post.cs
@@ -1,17 +1,28 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace blog.Models
 {
+    [Table("Post")]
     public class Post
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
-        public int CategoryId { get; set; }
-        public int AuthorId { get; set; }
         public string Title { get; set; }
         public string Summary { get; set; }
         public string Body { get; set; }
         public string Slug { get; set; }
         public DateTime CreateDate { get; set; }
         public DateTime LastUpdateDate { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+
+        [ForeignKey("AuthorId")]
+        public int AuthorId { get; set; }
+        public User Author { get; set; }
     }
 }
--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/User.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/User.cs
@@ -1,7 +1,13 @@
-﻿namespace blog.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace blog.Models
 {
+    [Table("User")]
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
