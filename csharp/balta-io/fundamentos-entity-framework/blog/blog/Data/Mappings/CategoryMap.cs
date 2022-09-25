bb2bb9acb7e473101acd3f25bf3b1280252f7717
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-25 13:28:01 -0400

    Mapping properties

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Data/Mappings/CategoryMap.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Data/Mappings/CategoryMap.cs
@@ -18,6 +18,19 @@ namespace blog.Data.Mappings
             builder.Property(x => x.Id)
                 .ValueGeneratedOnAdd()
                 .UseIdentityColumn();
+
+            // Propriedades
+            builder.Property(x => x.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("VARCHAR")
+                .HasMaxLength(80);
         }
     }
 }
