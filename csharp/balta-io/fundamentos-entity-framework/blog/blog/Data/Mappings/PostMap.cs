a4c985009d669bc1d90b0c4a3a46e8d3b4b8791e
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-25 16:21:53 -0400

    Many to many relationship

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Data/Mappings/PostMap.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Data/Mappings/PostMap.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 using System;
+using System.Collections.Generic;
 
 namespace blog.Data.Mappings
 {
@@ -42,6 +43,22 @@ namespace blog.Data.Mappings
                 .WithMany(x => x.Posts)
                 .HasConstraintName("FK_Post_Category")
                 .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, object>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_Post_Tag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
         }
     }
 }
