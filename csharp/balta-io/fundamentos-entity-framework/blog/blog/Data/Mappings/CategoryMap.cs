9d2a51a631378e442e8dd1523aabf7603bf3120e
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-25 13:21:42 -0400

    Starting the mapping

---
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,15 @@
+﻿using blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace blog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Tabela
+            builder.ToTable("Category");
+        }
+    }
+}
