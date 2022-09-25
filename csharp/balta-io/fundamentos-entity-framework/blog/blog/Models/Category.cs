7f723f196a75b41fb10d2e2e4bd55210cd11df68
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-24 23:16:29 -0400

    Table, Key and Identity

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/Category.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/Category.cs
@@ -1,7 +1,13 @@
-﻿namespace blog.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace blog.Models
 {
+    [Table("Category")]
     public class Category
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Slug { get; set; }
--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
@@ -11,56 +11,7 @@ namespace blog
         {
             using (var context = new BlogDataContext())
             {
-                // CREATE
-                //var tag = new Tag { Name = ".NET", Slug = "dotnet" };
-                //context.Tags.Add(tag);
-                //context.SaveChanges();
-
-                //var tag2 = new Tag { Name = "ASP.NET", Slug = "aspnet" };
-                //context.Tags.Add(tag2);
-                //context.SaveChanges();
-
-                // Update
-                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-                //tag.Name = ".NET";
-                //tag.Slug = "dotnet";
-
-                //context.Update(tag);
-                //context.SaveChanges();
-
-                // DELETE
-                //var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-
-                //context.Remove(tag);
-                //context.SaveChanges();
-
-                // ToList and AsNoTracking
-                //var tags = context
-                //    .Tags
-                //    .AsNoTracking()
-                //    .ToList();
-
-                //foreach (var tag in tags)
-                //{
-                //    System.Console.WriteLine(tag.Name);
-                //}
-
-                //var tag = context
-                //    .Tags
-                //    .AsNoTracking()
-                //    .FirstOrDefault(x => x.Id == 5);
-                //tag.Name = ".NET";
-                //tag.Slug = "dotnet";
-
-                //context.Update(tag);
-                //context.SaveChanges();
-
-                var tag = context
-                    .Tags
-                    .AsNoTracking()
-                    .SingleOrDefault(x => x.Id == 3);
-
-                System.Console.WriteLine(tag?.Name);
+                
             }
         }
     }
