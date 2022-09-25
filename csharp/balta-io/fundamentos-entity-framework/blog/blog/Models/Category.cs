cafdbc3c0c800eeb3dbcc7cf60d25bc32a017db7
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-24 23:19:47 -0400

    Required, MaxLenght and Column

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/Category.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Models/Category.cs
@@ -9,7 +9,17 @@ namespace blog.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "NVARCHAR")]
         public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Slug", TypeName = "VARCHAR")]
         public string Slug { get; set; }
     }
 }
