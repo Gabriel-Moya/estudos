5d05a41dc60ce678b11400500c4f0670726ebb80
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-25 16:43:20 -0400

    Testing the relationships

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace blog
+﻿using blog.Data;
+using blog.Models;
+using System.Linq;
+
+namespace blog
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            using var context = new BlogDataContext();
+
+            //context.Users.Add(new User
+            //{
+            //    Bio = "Uma biografia",
+            //    Email = "email@gmail.com",
+            //    Image = "https://site.com/imagem.jpeg",
+            //    Name = "Gabriel Moya",
+            //    PasswordHash = "1234",
+            //    Slug = "gabriel-moya"
+            //});
+            //context.SaveChanges();
+
+            var user = context.Users.FirstOrDefault();
+            var post = new Post
+            {
+                Author = user,
+                Body = "Meu artigo",
+                Category = new Category
+                {
+                    Name = "Backend",
+                    Slug = "backend"
+                },
+                CreateDate = System.DateTime.Now,
+                // LastUpdateDate=
+                Slug = "meu-artigo",
+                Summary = "Neste artigo vamos conferir...",
+                // Tags=null,
+                Title = "Meu artigo",
+            };
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
