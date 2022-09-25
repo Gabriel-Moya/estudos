c71d4527d2c6c941b98a9b2cf4deaec0b42a3bb1
Gabriel Moya <gabrielmoya123@gmail.com>
2022-09-24 23:38:11 -0400

    Working with subsets

--- csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
+++ csharp/balta-io/fundamentos-entity-framework/blog/blog/Program.cs
@@ -1,7 +1,6 @@
 ﻿using blog.Data;
 using blog.Models;
-using Microsoft.EntityFrameworkCore;
-using System.Linq;
+using System;
 
 namespace blog
 {
@@ -9,10 +8,38 @@ namespace blog
     {
         static void Main(string[] args)
         {
-            using (var context = new BlogDataContext())
+            using var context = new BlogDataContext();
+
+            var user = new User
             {
-                
-            }
+                Name = "Gabriel Moya",
+                Slug = "gabriel-moya",
+                Email = "gabrielmoya123@gmail.com",
+                Bio = "Descrição da biografia aqui",
+                Image = "https://enderecodaimagem.com/imagem.jpeg",
+                PasswordHash = "123456"
+            };
+
+            var category = new Category
+            {
+                Name = "Backend",
+                Slug = "backend"
+            };
+
+            var post = new Post
+            {
+                Author = user,
+                Category = category,
+                Body = "<p>Hello world!</p>",
+                Slug = "comecando-com-ef-core",
+                Summary = "Neste artigo vamos aprender EF Core",
+                Title = "Começando com EF Core",
+                CreateDate = DateTime.Now,
+                LastUpdateDate = DateTime.Now,
+            };
+
+            context.Posts.Add(post);
+            context.SaveChanges();
         }
     }
 }
