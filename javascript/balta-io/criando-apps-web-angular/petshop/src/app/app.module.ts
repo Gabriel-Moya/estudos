3d27acbe713668e7931bfe90b5a492365d3edf3b
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-01 23:28:48 -0400

    Login screen

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './signup-page/signup-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginPageComponent,
+    ResetPasswordPageComponent,
+    SignupPageComponent
   ],
   imports: [
     BrowserModule,
