dbe52e9f66c3fe0fa9e3a4b762072c77078424c7
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-09 17:55:27 -0400

    Services

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -26,6 +27,7 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/pages/store/products-page/products-page.component.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/pages/store/products-page/products-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-products-page',
@@ -6,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(data: DataService) { }
 
   ngOnInit(): void {
   }
