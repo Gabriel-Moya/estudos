97b37f3a825561da6aeb55aaca55bb9d1ab379b3
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-08 23:21:39 -0400

    Configuring the routes

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './pages/account/login-page/login-page.compon
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 
@@ -20,7 +21,8 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
     SignupPageComponent,
     ProductsPageComponent,
     PetsPageComponent,
-    CartPageComponent
+    CartPageComponent,
+    FramePageComponent
   ],
   imports: [
     BrowserModule,
