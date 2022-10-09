470b60ff22f0cb8e62fe7ce97ddd53ae437331fb
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-09 19:20:22 -0400

    Passing data to the component

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/components/store/product-card/product-card.component.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/components/store/product-card/product-card.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-product-card',
   templateUrl: './product-card.component.html'
 })
 export class ProductCardComponent implements OnInit {
+
+  @Input() product: any;
 
   constructor() { }
 
