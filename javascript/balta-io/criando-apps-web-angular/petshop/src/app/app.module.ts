023bcc21903d70df5d7faeef552d9bf3915b39e6
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-08 21:51:40 -0400

    Organizing the project

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
@@ -4,12 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { ResetPasswordPageComponent } from './reset-password-page/reset-password-page.component';
-import { SignupPageComponent } from './signup-page/signup-page.component';
-import { ProductsPageComponent } from './products-page/products-page.component';
-import { PetsPageComponent } from './pets-page/pets-page.component';
-import { CartPageComponent } from './cart-page/cart-page.component';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
+import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 
 @NgModule({
   declarations: [
--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/pages/account/signup-page/signup-page.component.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/pages/account/signup-page/signup-page.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-signup-page',
-  templateUrl: './signup-page.component.html',
-  styleUrls: ['./signup-page.component.css']
+  templateUrl: './signup-page.component.html'
 })
 export class SignupPageComponent implements OnInit {
 
