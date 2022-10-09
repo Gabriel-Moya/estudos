da946941b394a3aaf55a3b6e68e2d054cb6dbd59
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-08 22:31:06 -0400

    Master pages

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.component.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: '<router-outlet></router-outlet>'
 })
 export class AppComponent {
-  title = 'petshop';
+
 }
---
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/pages/master/frame.page.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-frame-page',
+  template: '<app-navbar></app-navbar><router-outlet></router-outlet>'
+})
+export class FramePageComponent {
+
+}
