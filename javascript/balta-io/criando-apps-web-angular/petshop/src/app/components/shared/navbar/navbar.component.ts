6057d064cc11366d2443fce812ff77734c9ea207
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-16 17:06:57 -0400

    Displaying logged in user

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/components/shared/navbar/navbar.component.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/components/shared/navbar/navbar.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/user.model';
+import { Security } from 'src/app/utils/security.util';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
+  public user: User;
 
   ngOnInit(): void {
+    this.user = Security.getUser();
   }
 
 }
