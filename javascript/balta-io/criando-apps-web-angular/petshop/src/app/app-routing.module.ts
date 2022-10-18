891003d6ce2d8d147d9b0a0caf611fb553d366b7
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-17 23:01:30 -0300

    Correct routes

--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/app-routing.module.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { PetsPageComponent } from './pages/account/pets-page/pets-page.component
 import { ProfilePageComponent } from './pages/account/profile-page/profile-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
-import { FramePageComponent } from './pages/master/frame.page';
+import { FramePageComponent } from './pages/master/frame-page.component';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { CheckoutPageComponent } from './pages/store/checkout-page/checkout-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
--- javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
+++ javascript/balta-io/criando-apps-web-angular/petshop/src/app/app.module.ts
@@ -1,40 +1,44 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FramePageComponent } from './pages/master/frame-page.component';
 import { LoginPageComponent } from './pages/account/login-page/login-page.component';
-import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
-import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
-import { FramePageComponent } from './pages/master/frame.page';
-import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
+import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
+import { CheckoutPageComponent } from './pages/store/checkout-page/checkout-page.component';
+import { ProfilePageComponent } from './pages/account/profile-page/profile-page.component';
+import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
+import { DataService } from './services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 import { ProductCardComponent } from './components/store/product-card/product-card.component';
 import { LoadingComponent } from './components/shared/loading/loading.component';
 import { MaskDirective } from './directives/mask.directive';
-import { DataService } from './services/data.service';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
 import { AuthService } from './services/auth.service';
 
 @NgModule({
   declarations: [
+    MaskDirective,
     AppComponent,
-    NavbarComponent,
-    LoginPageComponent,
-    ResetPasswordPageComponent,
-    SignupPageComponent,
-    ProductsPageComponent,
-    PetsPageComponent,
-    CartPageComponent,
     FramePageComponent,
+    LoginPageComponent,
+    SignupPageComponent,
+    ResetPasswordPageComponent,
+    ProductsPageComponent,
+    CartPageComponent,
+    CheckoutPageComponent,
+    ProfilePageComponent,
+    PetsPageComponent,
     ProductCardComponent,
     LoadingComponent,
-    MaskDirective
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
@@ -44,7 +48,7 @@ import { AuthService } from './services/auth.service';
     ToastrModule.forRoot(),
     AppRoutingModule
   ],
-  providers: [ DataService, AuthService ],
+  providers: [DataService, AuthService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
