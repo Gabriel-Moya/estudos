566295cffafc02b9f48613cf5ba764a8552b33c4
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-29 16:59:17 -0400

    Understanding the controller

---
+++ csharp/balta-io/fundamentos-aspnet-6/Todo/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Todo.Controllers;
+
+[ApiController]
+public class HomeController : ControllerBase
+{
+    [HttpGet]
+    public string Get()
+    {
+        return "Hello World!";
+    }
+}
