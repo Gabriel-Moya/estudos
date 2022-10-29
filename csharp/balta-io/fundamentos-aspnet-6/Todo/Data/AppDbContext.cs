e5afa5203bfee3c3586deb6c8c6b2cbd56ac1200
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-29 16:49:11 -0400

    Configurando o EF

---
+++ csharp/balta-io/fundamentos-aspnet-6/Todo/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Todo.Models;
+
+namespace Todo.Data;
+
+public class AppDbContext : DbContext
+{
+    public DbSet<TodoModel> Todos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseSqlite("DataSource=app.db;Cache=Shared");
+}
