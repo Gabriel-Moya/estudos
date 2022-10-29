f4704208b0480820801f7e519d00edd1f7c5a82b
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-29 17:05:36 -0400

    Add support the controllers

--- csharp/balta-io/fundamentos-aspnet-6/Todo/Program.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Todo/Program.cs
@@ -1,6 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+builder.Services.AddControllers();
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+app.MapControllers();
 
 app.Run();
