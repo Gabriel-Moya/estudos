6a3843d7c2c59aa616525117fab2c15689e80f90
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-29 20:02:30 -0400

    Updating and removing a registry

--- csharp/balta-io/fundamentos-aspnet-6/Todo/Controllers/HomeController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Todo/Controllers/HomeController.cs
@@ -14,6 +14,12 @@ public class HomeController : ControllerBase
         return context.Todos.ToList();
     }
 
+    [HttpGet("/{id:int}")]
+    public TodoModel GetById([FromRoute] int id, [FromServices] AppDbContext context)
+    {
+        return context.Todos.FirstOrDefault(x => x.Id == id);
+    }
+
     [HttpPost("/")]
     public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext context)
     {
@@ -21,5 +27,33 @@ public class HomeController : ControllerBase
         context.SaveChanges();
 
         return todo;
+    }
+
+    [HttpPut("/{id:int}")]
+    public TodoModel Put([FromRoute] int id,
+        [FromBody] TodoModel todo,
+        [FromServices] AppDbContext context)
+    {
+        var model = context.Todos.FirstOrDefault(x => x.Id == id);
+        if (model is null) return todo;
+
+        model.Title = todo.Title;
+        model.Done = todo.Done;
+
+        context.Todos.Update(model);
+        context.SaveChanges();
+
+        return model;
+    }
+
+    [HttpDelete("/{id:int}")]
+    public TodoModel Delete([FromRoute] int id, [FromServices] AppDbContext context)
+    {
+        var model = context.Todos.FirstOrDefault(x => x.Id == id);
+
+        context.Todos.Remove(model);
+        context.SaveChanges();
+
+        return model;
     }
 }
