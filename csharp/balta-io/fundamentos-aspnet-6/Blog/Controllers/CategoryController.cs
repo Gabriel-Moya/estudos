edd2224c935f3035e14ec900b80efd9cce0ed499
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-29 21:27:08 -0400

    Naming a endpoint

---
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/CategoryController.cs
@@ -0,0 +1,15 @@
+﻿using Blog.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers;
+
+[ApiController]
+public class CategoryController : ControllerBase
+{
+    [HttpGet("categories")]
+    public IActionResult Get([FromServices] BlogDataContext context)
+    {
+        var categories = context.Categories.ToList();
+        return Ok(categories);
+    }
+}
