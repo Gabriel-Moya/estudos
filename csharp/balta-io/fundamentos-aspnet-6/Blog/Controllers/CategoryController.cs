0cc10bac6272bbd0a9ac22802df26e3795a072b5
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-29 21:32:32 -0400

    Versioning

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/CategoryController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/CategoryController.cs
@@ -6,8 +6,15 @@ namespace Blog.Controllers;
 [ApiController]
 public class CategoryController : ControllerBase
 {
-    [HttpGet("categories")]
+    [HttpGet("v1/categories")] // localhost:PORT/v1/categories
     public IActionResult Get([FromServices] BlogDataContext context)
+    {
+        var categories = context.Categories.ToList();
+        return Ok(categories);
+    }
+
+    [HttpGet("v2/categories")] // localhost:PORT/v2/categories
+    public IActionResult Get2([FromServices] BlogDataContext context)
     {
         var categories = context.Categories.ToList();
         return Ok(categories);
