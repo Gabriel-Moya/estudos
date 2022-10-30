d6e1418cbab83e612f67e65f0c05f5745e458dda
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-30 15:42:32 -0400

    standardization

---
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/ViewModels/ResultViewModel.cs
@@ -0,0 +1,28 @@
+﻿namespace Blog.ViewModels;
+
+public class ResultViewModel<T>
+{
+    public ResultViewModel(T data, List<string> errors)
+    {
+        Data = data;
+        Errors = errors;
+    }
+
+    public ResultViewModel(T data)
+    {
+        Data = data;
+    }
+
+    public ResultViewModel(List<string> erros)
+    {
+        Errors = erros;
+    }
+
+    public ResultViewModel(string error)
+    {
+        Errors.Add(error);
+    }
+
+    public T Data { get; private set; }
+    public List<string> Errors { get; private set; } = new();
+}
