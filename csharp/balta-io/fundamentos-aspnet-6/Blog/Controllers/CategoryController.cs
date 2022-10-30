191640337bff5f406c2541e8354127cea3cf1ac0
Gabriel Moya <gabrielmoya123@gmail.com>
2022-10-30 11:27:50 -0400

    Finalizing the CRUD of the categories

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/CategoryController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/CategoryController.cs
@@ -11,17 +11,32 @@ public class CategoryController : ControllerBase
     [HttpGet("v1/categories")]
     public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
     {
-        var categories = await context.Categories.ToListAsync();
-        return Ok(categories);
+        try
+        {
+            var categories = await context.Categories.ToListAsync();
+            return Ok(categories);
+
+        }
+        catch
+        {
+            return StatusCode(500, "05X04 - Falha interna no servidor");
+        }
     }
 
     [HttpGet("v1/categories/{id:int}")]
     public async Task<IActionResult> GetByIdAsync([FromRoute] int id, [FromServices] BlogDataContext context)
     {
-        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
-        if (category is null) return NotFound();
-        return Ok(category);
+            if (category is null) return NotFound();
+            return Ok(category);
+        }
+        catch
+        {
+            return StatusCode(500, "05X05 - Falha interna no servidor");
+        }
     }
 
     [HttpPost("v1/categories")]
@@ -38,38 +53,60 @@ public class CategoryController : ControllerBase
         {
             return StatusCode(500, "05XE9 - Não foi possível incluir a categoria");
         }
-        catch (Exception ex)
+        catch
         {
-            return StatusCode(500, "05X10 - Falha interna no sergidor");
+            return StatusCode(500, "05X10 - Falha interna no servidor");
         }
     }
 
     [HttpPut("v1/categories/{id:int}")]
     public async Task<IActionResult> PutAsync([FromRoute] int id, [FromBody] Category model, [FromServices] BlogDataContext context)
     {
-        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
-        if (category is null) return NotFound();
+            if (category is null) return NotFound();
 
-        category.Name = model.Name;
-        category.Slug = model.Slug;
+            category.Name = model.Name;
+            category.Slug = model.Slug;
 
-        context.Categories.Update(category);
-        await context.SaveChangesAsync();
+            context.Categories.Update(category);
+            await context.SaveChangesAsync();
 
-        return Ok();
+            return Ok();
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "05XE8 - Não foi possível alterar a categoria");
+        }
+        catch
+        {
+            return StatusCode(500, "05X11 - Falha interna no servidor");
+        }
     }
 
     [HttpDelete("v1/categories/{id:int}")]
     public async Task<IActionResult> DeleteAsync([FromRoute] int id, [FromServices] BlogDataContext context)
     {
-        var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
+        try
+        {
+            var category = await context.Categories.FirstOrDefaultAsync(x => x.Id == id);
 
-        if (category is null) return NotFound();
+            if (category is null) return NotFound();
 
-        context.Categories.Remove(category);
-        await context.SaveChangesAsync();
+            context.Categories.Remove(category);
+            await context.SaveChangesAsync();
 
-        return Ok(category);
+            return Ok(category);
+        }
+        catch (DbUpdateException ex)
+        {
+            return StatusCode(500, "05XE7 - Não foi possível excluir a categoria");
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, "05X12 - Falha interna no sergidor");
+        }
     }
 }
