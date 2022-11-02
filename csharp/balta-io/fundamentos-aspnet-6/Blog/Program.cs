4287b8c251d0acd03b647e50472e951c1d859905
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-02 11:43:00 -0400

    AddScoped, AddTransient, AddSingleton

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Program.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Program.cs
@@ -1,4 +1,5 @@
 using Blog.Data;
+using Blog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,6 +11,10 @@ builder
         options.SuppressModelStateInvalidFilter = true;
     });
 builder.Services.AddDbContext<BlogDataContext>();
+//builder.Services.AddTransient();    // Sempre cria um novo
+//builder.Services.AddScoped();       // Cria um novo a cada transação (Request)
+//builder.Services.AddSingleton();    // Singleton => 1 por App
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
 app.MapControllers();
