f4622b7edcf7e30fbfc3f9da19d4977378ccda31
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-02 12:04:19 -0400

    JWT Claims

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Services/TokenService.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Services/TokenService.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Models;
+using System.Security.Claims;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
@@ -11,9 +12,15 @@ public class TokenService
     {
         var tokenHandler = new JwtSecurityTokenHandler();
         var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
-        
+
         var tokenDescriptor = new SecurityTokenDescriptor
         {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new (ClaimTypes.Name, "gabrielmoya"),   // User.Identity.Name
+                new (ClaimTypes.Role, "admin"),         // User.IsInRole
+                new ("texto", "valor")
+            }),
             Expires = DateTime.UtcNow.AddHours(8),
             SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
         };
