e99601dee74a6b69c1debbfe9255a00d9e54e6fb
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-02 14:30:10 -0400

    Initiating the users register

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
@@ -1,5 +1,8 @@
-﻿using Blog.Services;
-using Microsoft.AspNetCore.Authorization;
+﻿using Blog.Data;
+using Blog.Extensions;
+using Blog.Models;
+using Blog.Services;
+using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers;
@@ -7,23 +10,27 @@ namespace Blog.Controllers;
 [ApiController]
 public class AccountController : ControllerBase
 {
-    [HttpPost("v1/login")]
+    [HttpPost("v1/accounts")]
+    public async Task<IActionResult> Post([FromBody] RegisterViewModel model, [FromServices] BlogDataContext context)
+    {
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
+
+        var user = new User
+        {
+            Name = model.Name,
+            Email = model.Email,
+            Slug = model.Email.Replace("@", "-").Replace(".", "-")
+        };
+
+
+    }
+
+    [HttpPost("v1/accounts/login")]
     public IActionResult Login([FromServices]TokenService tokenService)
     {
         var token = tokenService.GenerateToken(null);
 
         return Ok(token);
     }
-
-    [Authorize(Roles = "user")]
-    [HttpGet("v1/user")]
-    public IActionResult GetUser() => Ok(User.Identity.Name);
-
-    [Authorize(Roles = "author")]
-    [HttpGet("v1/author")]
-    public IActionResult GetAuthor() => Ok(User.Identity.Name);
-
-    [Authorize(Roles = "admin")]
-    [HttpGet("v1/admin")]
-    public IActionResult GetAdmin() => Ok(User.Identity.Name);
 }
