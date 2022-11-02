86cfb0d4774c21fe9bf5d14eeee1a0093065e993
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-02 11:27:23 -0400

    Token service

---
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Services/TokenService.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace Blog.Services;
+
+public class TokenService
+{
+    public string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
