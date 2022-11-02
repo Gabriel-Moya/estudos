f4dae22e0bf5603be6622e3369f01b052bf07330
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-02 14:47:32 -0400

    Registering a new user

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
@@ -4,6 +4,8 @@ using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SecureIdentity.Password;
 
 namespace Blog.Controllers;
 
@@ -23,7 +25,27 @@ public class AccountController : ControllerBase
             Slug = model.Email.Replace("@", "-").Replace(".", "-")
         };
 
+        var password = PasswordGenerator.Generate(25);
+        user.PasswordHash = PasswordHasher.Hash(password);
 
+        try
+        {
+            await context.Users.AddAsync(user);
+            await context.SaveChangesAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                user = user.Email, password
+            }));
+        }
+        catch (DbUpdateException)
+        {
+            return StatusCode(400, new ResultViewModel<string>("05X99 - Este E-mail já está cadastrado"));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
     }
 
     [HttpPost("v1/accounts/login")]
