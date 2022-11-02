27b51f3d2c85090943926802faaf8d40bfcd24b8
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-02 13:51:05 -0400

    Configuring authenticate and authorization

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Program.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Program.cs
@@ -1,7 +1,27 @@
+using System.Text;
+using Blog;
 using Blog.Data;
 using Blog.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = false,
+        ValidateAudience = false
+    };
+});
 
 builder
     .Services
@@ -12,11 +32,15 @@ builder
     });
 builder.Services.AddDbContext<BlogDataContext>();
 //builder.Services.AddTransient();    // Sempre cria um novo
-//builder.Services.AddScoped();       // Cria um novo a cada transação (Request)
+//builder.Services.AddScoped();       // Cria um novo a cada transaï¿½ï¿½o (Request)
 //builder.Services.AddSingleton();    // Singleton => 1 por App
 builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
