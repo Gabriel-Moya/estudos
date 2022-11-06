889603421ac1a1770ba522d577ad62234d3744d4
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-05 22:21:37 -0400

    Authenticating

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
@@ -49,10 +49,32 @@ public class AccountController : ControllerBase
     }
 
     [HttpPost("v1/accounts/login")]
-    public IActionResult Login([FromServices]TokenService tokenService)
+    public async Task<IActionResult> Login([FromBody] LoginViewModel model,
+                               [FromServices] BlogDataContext context,
+                               [FromServices]TokenService tokenService)
     {
-        var token = tokenService.GenerateToken(null);
+        if (!ModelState.IsValid)
+            return BadRequest(new ResultViewModel<string>(ModelState.GetErrors()));
 
-        return Ok(token);
+        var user = await context.Users
+            .AsNoTracking()
+            .Include(x => x.Roles)
+            .FirstOrDefaultAsync(x => x.Email == model.Email);
+
+        if (user is null)
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+        if (!PasswordHasher.Verify(user.PasswordHash, model.Password))
+            return StatusCode(401, new ResultViewModel<string>("Usuário ou senha inválidos"));
+
+        try
+        {
+            var token = tokenService.GenerateToken(user);
+            return Ok(new ResultViewModel<string>(token, null));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
     }
 }
