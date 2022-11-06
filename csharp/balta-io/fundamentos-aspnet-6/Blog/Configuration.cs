9fb4276c821f2253a05a967a7f3a6881ef4505f8
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-05 23:42:58 -0400

    Implementing a ApiKey

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Configuration.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Configuration.cs
@@ -3,5 +3,7 @@
 public static class Configuration
 {
     // TOKEN - JWT
-    public static string JwtKey { get; set; } = "ZjZiMDdiNTAtMzQxOC00N2MxLWFiNDYtY2QxZjllYjgxMzg0";
+    public static string JwtKey = "ZjZiMDdiNTAtMzQxOC00N2MxLWFiNDYtY2QxZjllYjgxMzg0";
+    public static string ApiKeyName = "api_key";
+    public static string ApiKey = "curso_api_2MxLWFiNDYtY2QxZjllYvW023#$4a";
 }
