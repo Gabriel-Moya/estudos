ede5095ba68ac7a7bae48e4fe47fc5806ceda6e2
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-06 14:31:29 -0400

    Filtering data

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/PostController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.Models;
 using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
@@ -9,7 +10,7 @@ namespace Blog.Controllers;
 [ApiController]
 public class PostController : ControllerBase
 {
-    [HttpGet("v1´/posts")]
+    [HttpGet("v1/posts")]
     public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context,
                                               [FromQuery] int page = 0,
                                               [FromQuery] int pageSize = 25)
@@ -30,6 +31,72 @@ public class PostController : ControllerBase
                     LastUpdateDate = x.LastUpdateDate,
                     Category = x.Category.Name,
                     Author = $"{x.Author.Name} ({x.Author.Email})",
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("v1/posts/{id:int}")]
+    public async Task<IActionResult> DetailsAsync([FromServices] BlogDataContext context,
+                                                  [FromRoute] int id)
+    {
+        try
+        {
+            var post = await context.Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .ThenInclude(x => x.Roles)
+                .Include(x => x.Category)
+                .FirstOrDefaultAsync(x => x.Id == id);
+
+            if (post is null)
+                return NotFound(new ResultViewModel<Post>("Conteúdo não encontrado"));
+
+            return Ok(new ResultViewModel<Post>(post));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<Post>("05X04 - Falha interna no servidor"));
+        }
+    }
+
+    [HttpGet("v1/posts/category/{category}")]
+    public async Task<IActionResult> GetByCategoryAsync([FromRoute] string category,
+                                                        [FromServices] BlogDataContext context,
+                                                        [FromQuery] int page = 0,
+                                                        [FromQuery] int pageSize = 25)
+    {
+        try
+        {
+            var count = await context.Posts.AsNoTracking().CountAsync();
+            var posts = await context.Posts
+                .AsNoTracking()
+                .Include(x => x.Author)
+                .Include(x => x.Category)
+                .Where(x => x.Category.Slug == category)
+                .Select(x => new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})"
                 })
                 .Skip(page * pageSize)
                 .Take(pageSize)
