92af213bdf88e0691c52aa14738d9e56345cac6f
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-06 13:58:49 -0400

    Data pagination

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/PostController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/PostController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Data;
+using Blog.ViewModels;
 using Blog.ViewModels.Posts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -9,22 +10,43 @@ namespace Blog.Controllers;
 public class PostController : ControllerBase
 {
     [HttpGet("v1´/posts")]
-    public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context)
+    public async Task<IActionResult> GetAsync([FromServices] BlogDataContext context,
+                                              [FromQuery] int page = 0,
+                                              [FromQuery] int pageSize = 25)
     {
-        var posts = await context.Posts
-            .AsNoTracking()
-            .Include(x => x.Category)
-            .Include(x => x.Author)
-            //.Select(x =>
-            //new ListPostsViewModel {
-            //    Id = x.Id,
-            //    Title = x.Title,
-            //    Slug = x.Slug,
-            //    LastUpdateDate = x.LastUpdateDate,
-            //    Category = x.Category.Name,
-            //    Author = $"{x.Author.Name} ({x.Author.Email})",
-            //})
-            .ToListAsync();
-        return Ok(posts);
+        try
+        {
+            var count = await context.Posts.AsNoTracking().CountAsync();
+            var posts = await context.Posts
+                .AsNoTracking()
+                .Include(x => x.Category)
+                .Include(x => x.Author)
+                .Select(x =>
+                new ListPostsViewModel
+                {
+                    Id = x.Id,
+                    Title = x.Title,
+                    Slug = x.Slug,
+                    LastUpdateDate = x.LastUpdateDate,
+                    Category = x.Category.Name,
+                    Author = $"{x.Author.Name} ({x.Author.Email})",
+                })
+                .Skip(page * pageSize)
+                .Take(pageSize)
+                .OrderByDescending(x => x.LastUpdateDate)
+                .ToListAsync();
+
+            return Ok(new ResultViewModel<dynamic>(new
+            {
+                total = count,
+                page,
+                pageSize,
+                posts
+            }));
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<List<Post>>("05X04 - Falha interna no servidor"));
+        }
     }
 }
