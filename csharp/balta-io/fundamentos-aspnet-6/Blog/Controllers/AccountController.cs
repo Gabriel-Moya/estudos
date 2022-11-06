7ae52795c70a167cbcfd3b1044359f439a34fcef
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-06 11:45:56 -0400

    Upload image

--- csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/Controllers/AccountController.cs
@@ -4,9 +4,11 @@ using Blog.Models;
 using Blog.Services;
 using Blog.ViewModels;
 using Blog.ViewModels.Accounts;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SecureIdentity.Password;
+using System.Text.RegularExpressions;
 
 namespace Blog.Controllers;
 
@@ -84,5 +86,43 @@ public class AccountController : ControllerBase
         {
             return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
         }
+    }
+
+    [Authorize]
+    [HttpPost("v1/accounts/upload-image")]
+    public async Task<IActionResult> UploadImage([FromBody] UploadImageViewModel model,
+                                                 [FromServices] BlogDataContext context)
+    {
+        var fileName = $"{Guid.NewGuid().ToString()}.jpg";
+        var data = new Regex(@"^data:image\/[a-z]+;base64,").Replace(model.Base64Image, "");
+        var bytes = Convert.FromBase64String(data);
+
+        try
+        {
+            await System.IO.File.WriteAllBytesAsync($"wwwroot/images/{fileName}", bytes);
+        }
+        catch (Exception ex)
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
+
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Email == User.Identity.Name);
+
+        if (user is null)
+            return NotFound(new ResultViewModel<User>("Usuário não encontrado"));
+
+        user.Image = $"https://localhost:0000/images/{fileName}";
+
+        try
+        {
+            context.Users.Update(user);
+            await context.SaveChangesAsync();
+        }
+        catch
+        {
+            return StatusCode(500, new ResultViewModel<string>("05X04 - Falha interna no servidor"));
+        }
+
+        return Ok(new ResultViewModel<string>("Imagem alterada com sucesso!", null));
     }
 }
---
+++ csharp/balta-io/fundamentos-aspnet-6/Blog/ViewModels/Accounts/UploadImageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels.Accounts;
+
+public class UploadImageViewModel
+{
+    [Required(ErrorMessage = "Imagem inválida")]
+    public string Base64Image { get; set; }
+}
