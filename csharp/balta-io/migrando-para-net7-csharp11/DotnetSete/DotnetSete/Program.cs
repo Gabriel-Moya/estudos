19713c0555a7574b14459bd7d06569d18ad0e418
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-20 00:05:54 -0400

    List Patterns

--- csharp/balta-io/migrando-para-net7-csharp11/DotnetSete/DotnetSete/Program.cs
+++ csharp/balta-io/migrando-para-net7-csharp11/DotnetSete/DotnetSete/Program.cs
@@ -1,4 +1,6 @@
-﻿var text = $@"C:\dev"; // Verbatim
+﻿#region InterpolatedChain
+Console.WriteLine("Interpolated Chain:");
+var text = $@"C:\dev"; // Verbatim
 //var text2 = $"C:\dev Este é {text}"; // Non-verbatim
 
 var lista = new List<string>();
@@ -29,3 +31,26 @@ var location = $$"""
     You are at {{{longitude}}, {{latitude}}}
     """;
 Console.WriteLine(location);
+#endregion
+
+#region ListPatterns
+Console.WriteLine();
+Console.WriteLine("List Patterns:");
+int[] arr1 = { 1, 2, 10 };
+int[] arr2 = { 1, 2, 5, 10 };
+int[] arr3 = { 1, 2, 5, 6, 7, 8, 9, 10 };
+
+Console.WriteLine(CheckSwitch(arr1)); // 1
+Console.WriteLine(CheckSwitch(arr2)); // 1
+Console.WriteLine(CheckSwitch(arr3)); // 1
+
+static int CheckSwitch(int[] values)
+    => values switch
+    {
+        [1, 2, .., 10] => 1,
+        [1, 2] => 2,
+        [1, _] => 3,
+        [1, ..] => 4,
+        [..] => 50
+    };
+#endregion
