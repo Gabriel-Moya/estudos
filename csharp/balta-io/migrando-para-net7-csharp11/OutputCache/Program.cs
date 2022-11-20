824dbf94529254c2fb3b0577515507c5ec4fd231
Gabriel Moya <gabrielmoya123@gmail.com>
2022-11-20 14:27:22 -0400

    Improvement in queries

--- csharp/balta-io/migrando-para-net7-csharp11/OutputCache/Program.cs
+++ csharp/balta-io/migrando-para-net7-csharp11/OutputCache/Program.cs
@@ -21,7 +21,17 @@ app.MapGet("/", [EndpointSummary("Send a Hello request to the backend")] (MeuSer
     date = DateTime.Now
 }).WithDescription("Descrição do método get!");
 
-app.UseOutputCache();
+// Queries
+app.MapGet("/queries", (int[] q)
+    => $"q1: {q[0]}, q2: {q[1]}, q3: {q[2]}");
+
+// Tags
+app.MapGet("/tags", (Tag[] tags) =>
+{
+    return Results.Ok(tags);
+});
+
+//app.UseOutputCache();
 app.MapControllers();
 
 app.Run();
@@ -29,4 +39,23 @@ app.Run();
 public class MeuServico
 {
     public void DoSomething() { }
+}
+
+class Tag
+{
+    public string? TagName { get; init; }
+
+    public static bool TryParse(string? tagName, out Tag tag)
+    {
+        if (tagName is null)
+        {
+            tag = default;
+            return false;
+        }
+
+        tag = new Tag { TagName = tagName };
+        return true;
+    }
+
+    public override string ToString() => TagName ?? string.Empty;
 }
